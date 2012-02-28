9e61a81253b0cfe8b3f0f7b3b62e19583f85fc49
Michal Kamil Simon <michal.simon@cern.ch>
2012-02-28 16:07:05 +0000

    FTS-3 TRAC #67: Implement server side methods corresponding to client requests
    - log messages added

--- src/server/gsoap_transfer_methods.cpp
+++ src/server/gsoap_transfer_methods.cpp
@@ -25,6 +25,8 @@
 #include "config/serverconfig.h"
 #include "ws/FtsServiceTask.h"
 
+#include "common/logger.h"
+
 using namespace fts::ws;
 using namespace boost;
 using namespace db;
@@ -33,20 +35,28 @@ using namespace fts3::config;
 /// Web service operation 'transferSubmit' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::transferSubmit(transfer__TransferJob *_job, struct fts__transferSubmitResponse &_param_3) {
 
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'transferSubmit' request" << commit;
+
 	if (_job == 0) {
+
+		FTS3_COMMON_LOGGER_NEWLOG (ERR) << "The job was not defined" << commit;
 		transfer__InvalidArgumentException ex;
 		throw ex;
 	}
 
 	string id = UuidGenerator::generateUUID();
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Generated uuid " << id << commit;
+
 	try{
 	    const string requestID = id;
-	    const string dn ="/C=DE/O=GermanGrid/OU=DESY/CN=galway.desy.de";
+	    const string dn = "/C=DE/O=GermanGrid/OU=DESY/CN=galway.desy.de";
 	    const string vo = string("dteam");
 
 	    FtsServiceTask srvtask;
 
 	    vector<src_dest_checksum_tupple> jobs = srvtask.getJobs(_job);
+	    FTS3_COMMON_LOGGER_NEWLOG (DEBUG) << "Job's vector has been created" << commit;
+
 	    string sourceSpaceTokenDescription = "";
 
 	    string cred = "";
@@ -57,15 +67,20 @@ int FileTransferSoapBindingService::transferSubmit(transfer__TransferJob *_job, 
 
 	    vector<string> params = srvtask.getParams(_job->jobParams, copyPinLifeTime);
 
+	    FTS3_COMMON_LOGGER_NEWLOG (DEBUG) << "Parameter's vector has been created" << commit;
+
 	    DBSingleton::instance().getDBObjectInstance()->submitPhysical(requestID, jobs, params[0],
 	                                 dn, cred, vo, params[1],
 	                                 params[2], params[3], params[4],
 	                                 params[5], sourceSpaceTokenDescription, params[6], copyPinLifeTime,
 	                                 params[7], params[8]);
+
+	    FTS3_COMMON_LOGGER_NEWLOG (INFO) << "The job has been submitted" << commit;
 	  }
 	catch (string const &e)
 	  {
-	    cout << e << endl;
+	    FTS3_COMMON_LOGGER_NEWLOG (ERR) << "An exception has been thrown: " << e << commit;
+	    return SOAP_SVR_FAULT;
 	  }
 	_param_3._transferSubmitReturn = id;
 	return SOAP_OK;
@@ -74,21 +89,27 @@ int FileTransferSoapBindingService::transferSubmit(transfer__TransferJob *_job, 
 /// Web service operation 'transferSubmit2' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::transferSubmit2(transfer__TransferJob *_job, struct fts__transferSubmit2Response &_param_4) {
 
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'transferSubmit2' request" << commit;
+
 	if (_job == 0) {
+
+		FTS3_COMMON_LOGGER_NEWLOG (ERR) << "The job was not defined" << commit;
 		transfer__InvalidArgumentException ex;
 		throw ex;
 	}
 
 	string id = UuidGenerator::generateUUID();
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Generated uuid " << id << commit;
 
 	try{
 	    const string requestID = id;
-	    const string dn ="/C=DE/O=GermanGrid/OU=DESY/CN=galway.desy.de";
+	    const string dn = "/C=DE/O=GermanGrid/OU=DESY/CN=galway.desy.de";
 	    const string vo = string("dteam");
 
 	    FtsServiceTask srvtask;
 
 	    vector<src_dest_checksum_tupple> jobs = srvtask.getJobs(_job);
+	    FTS3_COMMON_LOGGER_NEWLOG (DEBUG) << "Job's vector has been created" << commit;
 
 	    string sourceSpaceTokenDescription = "";
 
@@ -96,20 +117,24 @@ int FileTransferSoapBindingService::transferSubmit2(transfer__TransferJob *_job,
 	    if (_job->credential) {
 	    	cred = *_job->credential;
 	    }
+
 	    int copyPinLifeTime = 1;
 
-
-
 	    vector<string> params = srvtask.getParams(_job->jobParams, copyPinLifeTime);
+	    FTS3_COMMON_LOGGER_NEWLOG (DEBUG) << "Parameter's vector has been created" << commit;
+
 	    DBSingleton::instance().getDBObjectInstance()->submitPhysical(requestID, jobs, params[0],
 	                                 dn, cred, vo, params[1],
 	                                 params[2], params[3], params[4],
 	                                 params[5], sourceSpaceTokenDescription, params[6], copyPinLifeTime,
 	                                 params[7], params[8]);
+
+	    FTS3_COMMON_LOGGER_NEWLOG (INFO) << "The job has been submitted" << commit;
 	  }
 	catch (string const &e)
 	  {
-	    cout << e << endl;
+	    FTS3_COMMON_LOGGER_NEWLOG (ERR) << "An exception has been thrown: " << e << commit;
+	    return SOAP_SVR_FAULT;
 	  }
 
 	_param_4._transferSubmit2Return = id;
@@ -119,20 +144,28 @@ int FileTransferSoapBindingService::transferSubmit2(transfer__TransferJob *_job,
 /// Web service operation 'transferSubmit3' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::transferSubmit3(transfer__TransferJob2 *_job, struct fts__transferSubmit3Response &_param_5) {
 
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'transferSubmit3' request" << commit;
+
 	if (_job == 0) {
+
+		FTS3_COMMON_LOGGER_NEWLOG (ERR) << "The job was not defined" << commit;
 		transfer__InvalidArgumentException ex;
 		throw ex;
 	}
 
 	string id = UuidGenerator::generateUUID();
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Generated uuid " << id << commit;
+
 	try{
 	    const string requestID = id;
-	    const string dn ="/C=DE/O=GermanGrid/OU=DESY/CN=galway.desy.de";
+	    const string dn = "/C=DE/O=GermanGrid/OU=DESY/CN=galway.desy.de";
 	    const string vo = string("dteam");
 
 	    FtsServiceTask srvtask;
 
 	    vector<src_dest_checksum_tupple> jobs = srvtask.getJobs2(_job);
+	    FTS3_COMMON_LOGGER_NEWLOG (DEBUG) << "Job's vector has been created" << commit;
+
 	    string sourceSpaceTokenDescription = "";
 
 	    string cred = "";
@@ -142,16 +175,20 @@ int FileTransferSoapBindingService::transferSubmit3(transfer__TransferJob2 *_job
 	    int copyPinLifeTime = 1;
 
 	    vector<string> params = srvtask.getParams(_job->jobParams, copyPinLifeTime);
+	    FTS3_COMMON_LOGGER_NEWLOG (DEBUG) << "Parameter's vector has been created" << commit;
 
 	    DBSingleton::instance().getDBObjectInstance()->submitPhysical(requestID, jobs, params[0],
 	                                 dn, cred, vo, params[1],
 	                                 params[2], params[3], params[4],
 	                                 params[5], sourceSpaceTokenDescription, params[6], copyPinLifeTime,
 	                                 params[7], params[8]);
+
+	    FTS3_COMMON_LOGGER_NEWLOG (INFO) << "The job has been submitted" << commit;
 	  }
 	catch (string const &e)
 	  {
-	    cout << e << endl;
+	    FTS3_COMMON_LOGGER_NEWLOG (ERR) << "An exception has been thrown: " << e << commit;
+	    return SOAP_SVR_FAULT;
 	  }
 	_param_5._transferSubmit3Return = id;
 
@@ -178,8 +215,11 @@ int FileTransferSoapBindingService::getFileStatus2(string _requestID, int _offse
 /// Web service operation 'getTransferJobStatus' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::getTransferJobStatus(string _requestID, struct fts__getTransferJobStatusResponse &_param_11) {
 
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'getTransferJobStatus' request" << commit;
+
 	try{
 		JobStatus* record =  DBSingleton::instance().getDBObjectInstance()->getTransferJobStatus(_requestID);
+		FTS3_COMMON_LOGGER_NEWLOG (INFO) << "The job status has been read" << commit;
 
 		if(record){
 
@@ -204,10 +244,13 @@ int FileTransferSoapBindingService::getTransferJobStatus(string _requestID, stru
 			_param_11._getTransferJobStatusReturn->numFiles = record->numFiles;
 			_param_11._getTransferJobStatusReturn->priority = record->priority;
 
+			FTS3_COMMON_LOGGER_NEWLOG (INFO) << "The response hav been created" << commit;
+
 			delete record;
 		}
 	} catch (string const &e) {
-		cout << e << endl;
+	    FTS3_COMMON_LOGGER_NEWLOG (ERR) << "An exception has been thrown: " << e << commit;
+	    return SOAP_SVR_FAULT;
 	}
 
 	return SOAP_OK;
@@ -227,24 +270,28 @@ int FileTransferSoapBindingService::getTransferJobSummary2(string _requestID, st
 
 /// Web service operation 'getVersion' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::getVersion(struct fts__getVersionResponse &_param_21) {
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'getVersion' request" << commit;
 	_param_21.getVersionReturn = "3.7.6-1";
 	return SOAP_OK;
 }
 
 /// Web service operation 'getSchemaVersion' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::getSchemaVersion(struct fts__getSchemaVersionResponse &_param_22) {
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'getSchemaVersion' request" << commit;
 	_param_22.getSchemaVersionReturn = "3.5.0";
 	return SOAP_OK;
 }
 
 /// Web service operation 'getInterfaceVersion' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::getInterfaceVersion(struct fts__getInterfaceVersionResponse &_param_23) {
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'getInterfaceVersion' request" << commit;
 	_param_23.getInterfaceVersionReturn = "3.7.0";
 	return SOAP_OK;
 }
 
 /// Web service operation 'getServiceMetadata' (returns error code or SOAP_OK)
 int FileTransferSoapBindingService::getServiceMetadata(string _key, struct fts__getServiceMetadataResponse &_param_24) {
+	FTS3_COMMON_LOGGER_NEWLOG (INFO) << "Handling 'getServiceMetadata' request" << commit;
 	_param_24._getServiceMetadataReturn = "glite-data-fts-service-3.7.6-1";
 	return SOAP_OK;
 }
