a4f5f3fc13bc11bd702f6a67ce165316e8f13836
Michail Salichos <michail.salichos@cern.ch>
2012-07-27 08:17:54 +0000

    introduce finisheddirty job state [ticket:259]

--- src/db/oracle/OracleAPI.cpp
+++ src/db/oracle/OracleAPI.cpp
@@ -1502,7 +1502,7 @@ void OracleAPI::updateJobTransferStatus(std::string file_id, std::string job_id,
 void OracleAPI::cancelJob(std::vector<std::string>& requestIDs){
 	const std::string cancelReason = "Job canceled by the user";
 	std::string cancelJ = "update t_job SET  JOB_STATE=:1, JOB_FINISHED =:2, FINISH_TIME=:3, REASON=:4 WHERE job_id = :5";
-	std::string cancelF = "update t_file set file_state=:1, JOB_FINISHED =:2, FINISH_TIME=:3, REASON=:4 WHERE JOB_ID=:5 AND file_state not in('FINISHED','FAILED')";
+	std::string cancelF = "update t_file set file_state=:1, JOB_FINISHED =:2, FINISH_TIME=:3, REASON=:4 WHERE JOB_ID=:5 AND file_state not in('FINISHEDDIRTY','FINISHED','FAILED')";
 	const std::string cancelJTag = "cancelJTag";
 	const std::string cancelFTag = "cancelFTag";
 	std::vector<std::string>::iterator iter;
