9af4bc368ce9bb513bcc2272d3bf0b924aea2e16
Michail Salichos <michail.salichos@cern.ch>
2012-11-06 11:35:59 +0000

    add missing files

---
+++ src/common/queue_updater.cpp
@@ -0,0 +1,94 @@
+/* Copyright @ Members of the EMI Collaboration, 2010.
+See www.eu-emi.eu for details on the copyright holders.
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. */
+
+#include <iostream>
+#include <time.h>
+#include "queue_updater.h"
+
+
+ThreadSafeList::ThreadSafeList() {    
+}
+
+ThreadSafeList::~ThreadSafeList() {
+}
+
+std::list<struct message_updater> ThreadSafeList::getList() {
+    ThreadTraits::LOCK_R lock(_mutex);
+    std::list<struct message_updater> tempList = m_list;   
+    return tempList;
+}
+
+void ThreadSafeList::push_back(struct message_updater msg) {
+    ThreadTraits::LOCK_R lock(_mutex);
+    m_list.push_back(msg);;
+}
+
+void ThreadSafeList::clear() {
+    ThreadTraits::LOCK_R lock(_mutex);
+    m_list.clear();
+}
+
+void ThreadSafeList::checkExpiredMsg(std::map<int, std::string>& pids) {
+    ThreadTraits::LOCK_R lock(_mutex);
+    std::list<struct message_updater>::const_iterator iter;
+    for (iter = m_list.begin(); iter != m_list.end(); ++iter) {
+        if (difftime(time(NULL), iter->timestamp) > 360) {
+            pids.insert(std::make_pair<int, std::string > (iter->file_id, std::string(iter->job_id)));
+        }
+    }    
+}
+
+void ThreadSafeList::updateMsg(struct message_updater *msg) {
+    ThreadTraits::LOCK_R lock(_mutex);
+    std::list<struct message_updater>::iterator iter;
+    for (iter = m_list.begin(); iter != m_list.end(); ++iter) {
+        if (msg->file_id == iter->file_id &&
+                std::string(msg->job_id).compare(std::string(iter->job_id)) == 0 &&
+                msg->process_id == iter->process_id) {
+            iter->timestamp = msg->timestamp;
+            break;
+        }
+    }    
+}
+
+void ThreadSafeList::deleteMsg(std::map<int, std::string>& pids) {
+    ThreadTraits::LOCK_R lock(_mutex);
+    std::list<struct message_updater>::iterator i = m_list.begin();
+    for (std::map< int, std::string>::const_iterator iter = pids.begin(); iter != pids.end(); ++iter) {
+        i = m_list.begin();
+        while (i != m_list.end()) {
+            if (iter->first == i->file_id &&
+                    std::string(iter->second).compare(std::string(i->job_id)) == 0) {
+                m_list.erase(i++);
+            } else {
+                ++i;
+            }
+        }
+    }
+}
+
+void ThreadSafeList::removeFinishedTr(std::string job_id, int file_id) {
+    ThreadTraits::LOCK_R lock(_mutex);
+    std::list<struct message_updater>::iterator i = m_list.begin();
+    while (i != m_list.end()) {
+        if (file_id == i->file_id &&
+                job_id.compare(std::string(i->job_id)) == 0) {
+            m_list.erase(i++);
+        } else {
+            ++i;
+        }
+    }    
+}
+
---
+++ src/common/queue_updater.h
@@ -0,0 +1,50 @@
+/* Copyright @ Members of the EMI Collaboration, 2010.
+See www.eu-emi.eu for details on the copyright holders.
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. */
+
+#ifndef THREADSAFELIST_H_
+#define THREADSAFELIST_H_
+
+#include <list>
+#include <map>
+#include <string>
+#include "definitions.h"
+#include "threadtraits.h"
+
+using namespace FTS3_COMMON_NAMESPACE;
+
+class ThreadSafeList {
+public:
+
+    static ThreadSafeList& get_instance() {
+        static ThreadSafeList instance;
+        return instance;
+    }
+    ThreadSafeList();
+    virtual ~ThreadSafeList();
+
+    std::list<struct message_updater> getList();
+    void push_back(struct message_updater msg);
+    void clear();
+    void updateMsg(struct message_updater *msg);
+    void checkExpiredMsg(std::map<int, std::string>& pids);
+    void deleteMsg(std::map<int, std::string>& pids);
+    void removeFinishedTr(std::string job_id, int file_id);
+
+private:
+    std::list<struct message_updater> m_list;
+    mutable ThreadTraits::MUTEX_R _mutex;    
+};
+
+#endif /*THREADSAFELIST_H_*/
