7892b0a8b1d7b983ea43c63bb19aa706a12b5462
Michal Kamil Simon <michal.simon@cern.ch>
2012-11-23 15:05:38 +0000

    #380 	Assign configuration to transfer job at submission time

--- src/db/schema/oracle-schema.sql
+++ src/db/schema/oracle-schema.sql
@@ -461,7 +461,7 @@ CREATE TABLE t_job (
   
   
 -- 
--- t_job_se_configuration the se configuration to be used by the job
+-- t_job_share_config the se configuration to be used by the job
 --
 CREATE TABLE t_job_share_config (
    job_id			CHAR(36) 		NOT NULL
@@ -469,8 +469,8 @@ CREATE TABLE t_job_share_config (
    ,destination     VARCHAR2(255)   NOT NULL
    ,vo 				VARCHAR2(100) 	NOT NULL
    ,CONSTRAINT t_job_share_config_pk PRIMARY KEY (job_id, source, destination, vo)
-   ,CONSTRAINT t_share_config_fk1 FOREIGN KEY (source, destination, vo) REFERENCES t_share_config (source, destination, vo)
-   ,CONSTRAINT t_share_config_fk2 FOREIGN KEY (job_id) REFERENCES t_job (job_id)
+   ,CONSTRAINT t_share_config_fk1 FOREIGN KEY (source, destination, vo) REFERENCES t_share_config (source, destination, vo) ON DELETE CASCADE
+   ,CONSTRAINT t_share_config_fk2 FOREIGN KEY (job_id) REFERENCES t_job (job_id) ON DELETE CASCADE
 );
   
   
