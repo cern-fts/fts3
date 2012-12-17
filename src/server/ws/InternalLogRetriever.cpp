f68c111e0534838705d37a38c3dd41c0571041c8
Michal Kamil Simon <michal.simon@cern.ch>
2012-12-17 11:02:30 +0000

    Ticket #415 Move LogStreamer to server/ws/transfer

--- src/server/ws/InternalLogRetriever.cpp
+++ src/server/ws/InternalLogRetriever.cpp
@@ -7,7 +7,7 @@
 
 #include "InternalLogRetriever.h"
 
-#include "ws/transfer/LogFileStreamer.h"
+//#include "ws/transfer/LogFileStreamer.h"
 
 //#include "ws-ifce/gsoap/fts3.nsmap"
 
@@ -24,9 +24,9 @@ InternalLogRetriever::InternalLogRetriever(string endpoint): endpoint(endpoint),
    	err = soap_cgsi_init(ctx,  CGSI_OPT_DISABLE_NAME_CHECK | CGSI_OPT_SSL_COMPATIBLE);
     soap_set_omode(ctx, SOAP_ENC_MTOM);
 
-	ctx->fmimewriteopen = LogFileStreamer::writeOpen;
-	ctx->fmimewriteclose = LogFileStreamer::writeClose;
-	ctx->fmimewrite = LogFileStreamer::write;
+//	ctx->fmimewriteopen = LogFileStreamer::writeOpen;
+//	ctx->fmimewriteclose = LogFileStreamer::writeClose;
+//	ctx->fmimewrite = LogFileStreamer::write;
 
     if (err) {
     	// TODO
@@ -47,7 +47,7 @@ InternalLogRetriever::~InternalLogRetriever() {
 list<string> InternalLogRetriever::getInternalLogs(string jobId) {
 
 	log__GetLogInternalResponse resp;
-	ctx->user = LogFileStreamer::getOutputHandler(resp);
+//	ctx->user = LogFileStreamer::getOutputHandler(resp);
 	soap_call_log__GetLogInternal(ctx, endpoint.c_str(), 0, jobId, resp);
 
 	list<string> ret (resp.logs->lognames.begin(), resp.logs->lognames.end());
