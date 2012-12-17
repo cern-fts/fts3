a2733692dc15508b21afbc527569a4637b31f4fe
Michal Kamil Simon <michal.simon@cern.ch>
2012-12-17 10:55:57 +0000

    Ticket #415 Move LogStreamer to server/ws/transfer

--- src/server/ws/InternalLogRetriever.cpp
+++ src/server/ws/InternalLogRetriever.cpp
@@ -7,14 +7,13 @@
 
 #include "InternalLogRetriever.h"
 
-#include "ws-ifce/LogFileStreamer.h"
+#include "transfer/LogFileStreamer.h"
 
 //#include "ws-ifce/gsoap/fts3.nsmap"
 
 #include <cgsi_plugin.h>
 
 using namespace fts3::ws;
-using namespace fts3::common;
 
 InternalLogRetriever::InternalLogRetriever(string endpoint): endpoint(endpoint), ctx(soap_new1(SOAP_ENC_MTOM)) {
 	// TODO Auto-generated constructor stub
