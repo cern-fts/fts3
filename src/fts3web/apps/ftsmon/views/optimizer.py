f572a3785c3d3d215447d4bd230051908f181897
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-02-19 10:18:36 +0000

    Optimizer tab: Show only max throughputs

--- src/fts3web/apps/ftsmon/views/optimizer.py
+++ src/fts3web/apps/ftsmon/views/optimizer.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.db.models import Max
 from fts3.models import Optimize
 from ftsmon import forms
+
 
 def optimizer(httpRequest):
     # Initialize forms
@@ -14,7 +16,18 @@ def optimizer(httpRequest):
             optimizations = optimizations.filter(source_se = filterForm['source_se'].value())
         if filterForm['dest_se'].value():
             optimizations = optimizations.filter(dest_se = filterForm['dest_se'].value())
+            
+    # Only max!   
+    optimizations = optimizations.extra(where = [
+		""" "T_OPTIMIZE"."THROUGHPUT" = (SELECT MAX("O2"."THROUGHPUT") FROM T_OPTIMIZE O2
+		                                 WHERE "O2"."SOURCE_SE" = "T_OPTIMIZE"."SOURCE_SE" AND
+		                                       "O2"."DEST_SE" = "T_OPTIMIZE"."DEST_SE" AND
+		                                       "O2"."ACTIVE" = "T_OPTIMIZE"."ACTIVE")"""])
     
+    optimizations = optimizations.order_by('source_se', 'dest_se', 'active')
+    
+    
+    print optimizations.query
     
     return render(httpRequest, 'optimizer.html',
               {'request': httpRequest,
