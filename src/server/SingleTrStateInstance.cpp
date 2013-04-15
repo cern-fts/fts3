c199cff749f96e30aa1240dc01f75e9ec690412f
Michail Salichos <michail.salichos@cern.ch>
2013-04-15 09:41:37 +0000

    first commit for state messages

---
+++ src/server/SingleTrStateInstance.cpp
@@ -0,0 +1,131 @@
+/********************************************//**
+ * Copyright @ Members of the EMI Collaboration, 2010.
+ * See www.eu-emi.eu for details on the copyright holders.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ ***********************************************/
+
+#include "SingleTrStateInstance.h"
+#include "logger.h"
+#include "error.h"
+#include "producer_consumer_common.h"
+#include "db/generic/SingleDbInstance.h"
+#include <sstream>
+
+using namespace db;
+
+std::string _getTrTimestampUTC(){
+        time_t now = time(NULL);
+        struct tm tTime;
+        gmtime_r(&now, &tTime);
+        time_t msec = mktime(&tTime) * 1000; //the number of milliseconds since the epoch
+        std::ostringstream oss;
+        oss << fixed << msec;
+        return oss.str();
+}
+
+
+
+boost::scoped_ptr<SingleTrStateInstance> SingleTrStateInstance::i;
+ThreadTraits::MUTEX SingleTrStateInstance::_mutex;
+
+// Implementation
+
+SingleTrStateInstance::SingleTrStateInstance(){
+
+}
+
+SingleTrStateInstance::~SingleTrStateInstance() {
+}
+
+
+void SingleTrStateInstance::sendStateMessage(const std::string& jobId, int fileId){
+  struct message_state state;
+  
+ try {
+ 	if(fileId != -1){
+        	state  = db::DBSingleton::instance().getDBObjectInstance()->getStateOfTransfer(jobId, fileId);
+		constructJSONMsg(&state);
+	}else{ //need to get file_id for the given job
+		std::vector<int> files;
+		db::DBSingleton::instance().getDBObjectInstance()->getFilesForJob(jobId, files);
+		if(!files.empty()){
+			std::vector<int>::iterator it;   
+    			for (it = files.begin(); it != files.end(); ++it) {  
+				state  = db::DBSingleton::instance().getDBObjectInstance()->getStateOfTransfer(jobId, (*it));
+				constructJSONMsg(&state);		
+			}
+		}		
+	}				
+    } catch (Err& e) {
+        throw;
+    } catch (std::exception& ex) {
+        throw;
+    } catch (...) {
+        throw;
+    }   
+}
+
+void SingleTrStateInstance::sendStateMessage(  const std::string&  vo_name, const std::string&  source_se, const std::string&  dest_se, const std::string&  job_id, int file_id, const std::string& 
+  job_state, const std::string&  file_state, int retry_counter, int retry_max, const std::string&  job_metadata, const std::string&  file_metadata){
+   struct message_state state;
+   state.vo_name = vo_name;
+   state.source_se = source_se;
+   state.dest_se = dest_se;
+   state.job_id = job_id;
+   state.file_id = file_id;
+   state.job_state = job_state;
+   state.file_state = file_state;
+   state.retry_counter = retry_counter;
+   state.retry_max = retry_max;
+   state.job_metadata = job_metadata;
+   state.file_metadata = file_metadata;
+   state.timestamp = _getTrTimestampUTC();  
+  
+   constructJSONMsg(&state);
+}
+  
+
+void SingleTrStateInstance::constructJSONMsg(struct message_state* state){
+
+	std::ostringstream json_message;
+	json_message << "SS {";
+	json_message << "\"vo_name\":" << "\"" << state->vo_name << "\",";
+	json_message << "\"source_se\":" << "\"" << state->source_se << "\",";	
+	json_message << "\"dest_se\":" << "\"" << state->dest_se << "\",";	      
+	json_message << "\"job_id\":" << "\"" << state->job_id << "\",";	      	
+	json_message << "\"file_id\":" << "\"" << state->file_id << "\",";	      		
+	json_message << "\"job_state\":" << "\"" << state->job_state << "\",";	      			
+	json_message << "\"file_state\":" << "\"" << state->file_state << "\",";	      				
+	json_message << "\"retry_counter\":" << "\"" << state->retry_counter << "\",";	      					
+	json_message << "\"retry_max\":" << "\"" << state->retry_max << "\",";
+	if(state->job_metadata.length() > 0 )
+		json_message << "\"job_metadata\":" << state->job_metadata << ",";
+	else
+		json_message << "\"job_metadata\":\"\",";
+	if(state->file_metadata.length() > 0 )
+		json_message << "\"file_metadata\":" << state->file_metadata << ",";
+	else
+		json_message << "\"file_metadata\":\"\",";
+	json_message << "\"timestamp\":" << "\"" << state->timestamp << "\"";	      									
+	json_message << "}";		
+	
+	struct message_monitoring message;
+	strcpy(message.msg, std::string(json_message.str()).c_str());
+	message.timestamp = milliseconds_since_epoch();   	
+	runProducerMonitoring( message );
+}
+
+
+
+
---
+++ src/server/SingleTrStateInstance.h
@@ -0,0 +1,76 @@
+/********************************************//**
+ * Copyright @ Members of the EMI Collaboration, 2010.
+ * See www.eu-emi.eu for details on the copyright holders.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ ***********************************************/
+
+#pragma once
+
+
+#include <iostream>
+#include <boost/scoped_ptr.hpp>
+#include "common/monitorobject.h"
+#include "GenericDbIfce.h"
+#include "DynamicLibraryManager.h"
+#include "MonitoringDbIfce.h"
+#include "definitions.h"
+#include "server_dev.h"
+
+
+
+using namespace FTS3_COMMON_NAMESPACE;
+
+/**
+ * SingleTrStateInstance class declaration
+ **/ 
+class SingleTrStateInstance: public MonitorObject {
+public:
+    ~SingleTrStateInstance();
+
+/**
+ * SingleTrStateInstance thread-safe upon init singleton
+ **/ 
+    static SingleTrStateInstance & instance() {
+        if (i.get() == 0) {
+            ThreadTraits::LOCK lock(_mutex);
+            if (i.get() == 0) {
+                i.reset(new SingleTrStateInstance);
+            }	    
+        }
+        return *i;
+    }
+
+
+  void sendStateMessage(const std::string& jobId, int fileId);
+  
+  void sendStateMessage(  const std::string&  vo_name, const std::string&  source_se, const std::string&  dest_se, const std::string&  job_id, int file_id, const std::string& 
+  job_state, const std::string&  file_state, int retry_counter, int retry_max, const std::string&  job_metadata, const std::string&  file_metadata);
+  
+ 
+
+
+private:
+    SingleTrStateInstance(); // Private so that it can  not be called
+
+    SingleTrStateInstance(SingleTrStateInstance const&) {
+    }; // copy constructor is private
+    static ThreadTraits::MUTEX _mutex;
+    SingleTrStateInstance & operator=(SingleTrStateInstance const&);
+    // assignment operator is private
+    static boost::scoped_ptr<SingleTrStateInstance> i; 
+    
+    void constructJSONMsg(struct message_state* state);
+};
+
+
