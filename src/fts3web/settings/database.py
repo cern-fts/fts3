90821b2a40a0b4596d4a8fe73952a19982eabd06
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-04-22 07:25:36 +0000

    Avoid locking reads in MySQL

--- src/fts3web/settings/database.py
+++ src/fts3web/settings/database.py
@@ -7,9 +7,13 @@ DATABASES = {
         'PASSWORD': FTS3WEB_CONFIG.get('database', 'password'),
         'NAME':     FTS3WEB_CONFIG.get('database', 'name'),
         'HOST':     FTS3WEB_CONFIG.get('database', 'host'),
-        'PORT':     FTS3WEB_CONFIG.get('database', 'port')
+        'PORT':     FTS3WEB_CONFIG.get('database', 'port'),
+        'OPTIONS':  {}
     }
 }
 
 if DATABASES['default']['ENGINE'] == 'django.db.backends.oracle':
-    DATABASES['default']['OPTIONS'] = {'threaded': True}
+    DATABASES['default']['OPTIONS']['threaded'] = True
+
+if DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+    DATABASES['default']['OPTIONS']['init_command'] = "SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED"
