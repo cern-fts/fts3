2980f06c80aea856c9b147a47a5a1c9828d027ca
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-05-16 13:14:45 +0000

    Removed t_site_group from mysql-truncate.sql

--- src/db/schema/mysql-truncate.sql
+++ src/db/schema/mysql-truncate.sql
@@ -6,7 +6,6 @@ TRUNCATE t_job_share_config;
 TRUNCATE t_job;
 TRUNCATE t_vo_acl;
 TRUNCATE t_se_pair_acl;
-TRUNCATE t_site_group;
 TRUNCATE t_bad_dns;
 TRUNCATE t_bad_ses;
 TRUNCATE t_share_config;
@@ -19,6 +18,6 @@ TRUNCATE t_credential_cache;
 TRUNCATE t_debug;
 TRUNCATE t_config_audit;
 TRUNCATE t_optimize;
-TRUNCATE t_optimizer_evolution
+TRUNCATE t_optimizer_evolution;
 TRUNCATE t_server_config;
 INSERT INTO t_server_config (retry,max_time_queue) values(0,0);
