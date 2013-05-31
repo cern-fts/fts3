57528522ec0ae7abe6fa866916b82c41955361f0
Michal Kamil Simon <michal.simon@cern.ch>
2013-05-31 13:01:37 +0000

    Ticket #593 Optimize slow Oracle queries

--- src/db/schema/oracle-schema.sql
+++ src/db/schema/oracle-schema.sql
@@ -680,6 +680,7 @@ CREATE INDEX file_file_index ON t_file(file_index);
 CREATE INDEX file_retry_timestamp ON t_file(retry_timestamp);
 CREATE INDEX file_file_throughput ON t_file(throughput);
 CREATE INDEX file_file_src_dest ON t_file(source_se, dest_se);
+CREATE INDEX file_file_src_dest_job_id ON t_file(source_se, dest_se, job_id);
 CREATE INDEX file_file_state_job_id4 ON t_file(file_state, dest_se);
 
 
