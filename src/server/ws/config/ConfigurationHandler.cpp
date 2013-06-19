2eb3757cd0baab8dd690c0a5df36902ff1f455a8
Michal Kamil Simon <michal.simon@cern.ch>
2013-06-19 15:05:49 +0000

    Ticket #624 "A configuration for source: '*' and destination: 'srm://atlassrm-fzk.gridka.de' does not exist!"

--- src/server/ws/config/ConfigurationHandler.cpp
+++ src/server/ws/config/ConfigurationHandler.cpp
@@ -216,6 +216,7 @@ vector<string> ConfigurationHandler::getAll(string name)
 
     try
     {
+    	// get the standalone configuration
     	ret.push_back(get(name));
     }
     catch (Err& ex)
@@ -223,11 +224,30 @@ vector<string> ConfigurationHandler::getAll(string name)
     	// we don't care if nothing has been found
     }
 
+    // check if there are pair configurations
     vector< pair<string, string> > pairs = db->getPairsForSe(name);
     vector< pair<string, string> >::iterator it;
 
     for (it = pairs.begin(); it != pairs.end(); it++) {
+    	// get the pair configurations
     	ret.push_back(getPair(it->first, it->second));
+    }
+
+    // check if it is a member of a group
+    string group = db->getGroupForSe(name);
+
+    if (!group.empty()) {
+
+       	// get the standalone group configuration
+       	ret.push_back(get(group));
+
+        // check if there are pair configurations
+        pairs = db->getPairsForSe(group);
+
+        for (it = pairs.begin(); it != pairs.end(); it++) {
+        	// get the pair configurations
+        	ret.push_back(getPair(it->first, it->second));
+        }
     }
 
     return ret;
