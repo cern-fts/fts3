9674fe415333b5e326c91f71eae42925925cf103
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-07-04 14:58:31 +0000

    Removed useless order by

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -1675,8 +1675,7 @@ void MySqlAPI::fetchOptimizationConfig2(OptimizerSample* ops, const std::string 
                 " 	AND t_file.reason LIKE '%operation timeout%' "
                 "	AND t_file.source_se = :sourceSe "
                 " 	AND t_file.dest_se = :destSe  "
-                " 	AND (t_file.finish_time > (UTC_TIMESTAMP - INTERVAL '30' minute)) "
-                " ORDER BY t_file.finish_time DESC",
+                " 	AND (t_file.finish_time > (UTC_TIMESTAMP - INTERVAL '30' minute))",
                 soci::use(source_hostname),
                 soci::use(destin_hostname),
                 soci::into(timeoutTr)
