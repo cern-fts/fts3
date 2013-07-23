01c8516f3af3370caad6c28926c8cf041b7bfd94
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-07-23 11:27:37 +0000

    Ticket #665: Add filtering to the queue

--- src/fts3web/apps/ftsmon/views/queue.py
+++ src/fts3web/apps/ftsmon/views/queue.py
@@ -2,12 +2,25 @@ from ftsweb.models import File
 from django.db.models import Count
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from ftsmon import forms
+from jobs import setupFilters
 from urllib import urlencode
 from utils import getPage
 
 
 def queueByPairs(httpRequest):
+    filterForm = forms.FilterForm(httpRequest.GET)
+    filters    = setupFilters(filterForm)
+    
     pairs = File.objects.filter(file_state__in = ['SUBMITTED', 'READY'])
+    
+    if filters['vo']:
+        pairs = pairs.filter(job__vo_name = filters['vo'])
+    if filters['source_se']:
+        pairs = pairs.filter(source_se = filters['source_se'])
+    if filters['dest_se']:
+        pairs = pairs.filter(dest_se = filters['dest_se'])
+    
     pairs = pairs.values('source_se', 'dest_se', 'job__vo_name')
     pairs = pairs.annotate(ntransfers = Count('file_id'))
     pairs = pairs.order_by('source_se', 'dest_se', 'job__vo_name')
@@ -16,34 +29,43 @@ def queueByPairs(httpRequest):
     return render(httpRequest, 'queue/queue.html',
                   {'pairsPerVo': getPage(paginator, httpRequest),
                    'paginator': paginator,
-                   'request': httpRequest})
+                   'request': httpRequest,
+                   'filterForm': filterForm})
 
 
 
 def queueDetailed(httpRequest):
-    source_se = httpRequest.GET['source_se']
-    dest_se   = httpRequest.GET['dest_se']
-    vo_name   = httpRequest.GET['vo']
+    filterForm = forms.FilterForm(httpRequest.GET)
+    filters    = setupFilters(filterForm)
     
     transfers = File.objects.filter(file_state__in = ['SUBMITTED', 'READY'])
-    transfers = transfers.filter(source_se = source_se, dest_se = dest_se, job__vo_name = vo_name)
+    transfers = transfers.filter(source_se = filters['source_se'],
+                                 dest_se = filters['dest_se'],
+                                 job__vo_name = filters['vo'])
     
-    extra_args = urlencode({'source_se': source_se,
-                            'dest_se': dest_se,
-                            'vo': vo_name})
+    print transfers.query
+    
+    extra_args = urlencode({'source_se': filters['source_se'],
+                            'dest_se': filters['dest_se'],
+                            'vo': filters['vo']})
     
     paginator = Paginator(transfers, 50)
     return render(httpRequest, 'queue/details.html',
                   {'transfers': getPage(paginator, httpRequest),
                    'paginator': paginator,
                    'request': httpRequest,
-                   'extra_args': extra_args})
+                   'extra_args': extra_args,
+                   'filters': filters,
+                   'filterForm': filterForm})
 
 
 
 def queue(httpRequest):
     get = httpRequest.GET
-    if reduce(lambda a,b: a and b, map(lambda k: k in get, ['source_se', 'dest_se', 'vo'])):
+    
+    filteredFields = ['source_se', 'dest_se', 'vo']
+    
+    if reduce(lambda a,b: a and b, map(lambda k: k in get and get[k], filteredFields)):
         return queueDetailed(httpRequest)
     else:
         return queueByPairs(httpRequest)
