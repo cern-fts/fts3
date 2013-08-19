fba05437abfe1f7a488f7ab076bd9d4939010cd8
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-08-19 07:51:47 +0000

    Unrolled tests to get more granularity in the results

--- test/functional/lib/base.py
+++ test/functional/lib/base.py
@@ -1,14 +1,12 @@
 import collections
 import logging
 import types
-import unittest
 import cli, storage, surl 
 
 
-class TestBase(unittest.TestCase):
+class TestBase():
 
 	def __init__(self, *args, **kwargs): 
-		super(TestBase, self).__init__(*args, **kwargs)
 		self.surl   = surl.Surl()
 		self.client = cli.Cli()
 
@@ -41,5 +39,37 @@ class TestBase(unittest.TestCase):
 				self.surl.unlink(f)
 			except Exception, e:
 				logging.warning(e)
+
+
+
+	def run(self):
+		testMethods = [getattr(self, method) for method in dir(self) if method.startswith('test_') and callable(getattr(self, method))]
+		nErrors = 0
+		for test in testMethods:
+			try:
+				self.setUp()
+				test()
+			except Exception, e:
+				logging.error(str(e))
+				nErrors += 1
+			self.tearDown()
+		logging.info("%d test executed, %d failed" % (len(testMethods), nErrors))
+		return nErrors
+
+
+	def assertEqual(self, expected, value):
+		if expected != value:
+			raise AssertionError("Expected '%s', got '%s'" % (str(expected), str(value)))
+		logging.info("Assertion '%s' == '%s' OK" % (str(expected), str(value)))
+
+
+	def assertRaises(self, excType, method, *args, **kwargs):
+		try:
+			method(*args, **kwargs)
+			raise AssestionError("Expected exception '%s', none raised" % (excType.__name__))
+		except Exception, e:
+			if not isinstance(e, excType):
+				raise AssertionError("Expected exception '%s', got '%s'" % (excType.__name__, type(e).__name__))
+			logging.info("Exception '%s' raised as expected" % excType.__name__)
 
 
