bb4baf21e661ef33cb6f8c07cc057b4d2992febf
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-08-27 10:46:16 +0000

    Monitoring: Fixes in per pair stats

--- src/fts3web/apps/ftsmon/views/statistics.py
+++ src/fts3web/apps/ftsmon/views/statistics.py
@@ -279,7 +279,7 @@ def pairs(httpRequest):
     aggregate = {}
     aggregate['active']        = reduce(lambda a,b: _sumStatus(a, b),  map(lambda x: x['active'], pairs), {})
     aggregate['successRate']   = _avgField(pairs, 'successRate')
-    aggregate['avgThroughput'] = reduce(lambda a, b: a + b, [x['avgThroughput'] for x in pairs if x['avgThroughput']], 0)
+    aggregate['avgThroughput'] = _avgField(pairs, 'avgThroughput')
     aggregate['avgDuration']   = _avgField(pairs, 'avgDuration')
     
     return {'pairs': pairs,
