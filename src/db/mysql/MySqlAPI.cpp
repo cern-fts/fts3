f12935198aa39fbb4bc8d74ae8926bdd1a03c91f
Michail Salichos <michail.salichos@cern.ch>
2013-09-11 18:03:13 +0000

    one more attempt to optimize query

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -295,9 +295,8 @@ void MySqlAPI::getByJobId(std::map< std::string, std::list<TransferFiles*> >& fi
                     soci::row const& rVO = *iVO;
                     std::string vo_name = rVO.get<std::string>("vo_name");
                     soci::rowset<soci::row> rs = (
-                                                     sql.prepare << " select distinct f.source_se, f.dest_se from t_file f IGNORE INDEX(file_state_dest_surl) where "
-						     		    " f.file_state='SUBMITTED' AND  exists (select null from t_job j where "
-								    " j.vo_name=:vo_name and f.job_id=j.job_id) ",
+                                                     sql.prepare << " select  distinct f.source_se, f.dest_se from t_file f IGNORE INDEX(file_state_dest_surl)  "
+						     		    " INNER JOIN t_job j ON (f.job_id = j.job_id) and j.vo_name = :vo_name and f.file_state='SUBMITTED'",
                                                      soci::use(vo_name)
                                                  );
                     for (soci::rowset<soci::row>::const_iterator i = rs.begin(); i != rs.end(); ++i)
