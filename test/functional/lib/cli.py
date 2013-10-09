8bcf7944a1ac32b2210082746ca4aa0f8966e3de
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-10-09 08:08:20 +0000

    Ticket #602: Test fts-multihop checks the final state of the individual hops

--- test/functional/lib/cli.py
+++ test/functional/lib/cli.py
@@ -11,75 +11,96 @@ import time
 
 class Cli:
 
-	def submit(self, transfers, extraArgs = []):
-		"""
-		Spawns a transfer and returns the job ID
-		"""
-		# Build the submission file
-		submission = tempfile.NamedTemporaryFile(delete = False, suffix = '.submission')
-		submission.write(json.dumps({'Files': transfers}))
-		submission.close()
+    def submit(self, transfers, extraArgs = []):
+        """
+        Spawns a transfer and returns the job ID
+        """
+        # Build the submission file
+        submission = tempfile.NamedTemporaryFile(delete = False, suffix = '.submission')
+        submission.write(json.dumps({'Files': transfers}))
+        submission.close()
 
-		# Label the job
-		caller = inspect.stack()[1][3]
-		labeldict = {'label': config.TestLabel, 'test': caller}
-		label = json.dumps(labeldict)
+        # Label the job
+        caller = inspect.stack()[1][3]
+        labeldict = {'label': config.TestLabel, 'test': caller}
+        label = json.dumps(labeldict)
 
-		# Spawn the transfer
-		cmdArray = ['fts-transfer-submit',
+        # Spawn the transfer
+        cmdArray = ['fts-transfer-submit',
                     '-s', config.Fts3Endpoint,
                     '--job-metadata', label, 
-					'--new-bulk-format', '-f', submission.name] + extraArgs
-		logging.debug("Spawning %s" % ' '.join(cmdArray))
-		proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-		rcode = proc.wait()
-		if rcode != 0:
-			logging.error(proc.stdout.read())
-			logging.error(proc.stderr.read())
-			raise Exception("fts-transfer-submit failed with exit code %d" % rcode)
-		jobId = proc.stdout.read().strip()
+                    '--new-bulk-format', '-f', submission.name] + extraArgs
+        logging.debug("Spawning %s" % ' '.join(cmdArray))
+        proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        rcode = proc.wait()
+        if rcode != 0:
+            logging.error(proc.stdout.read())
+            logging.error(proc.stderr.read())
+            raise Exception("fts-transfer-submit failed with exit code %d" % rcode)
+        jobId = proc.stdout.read().strip()
 
-		os.unlink(submission.name)
+        os.unlink(submission.name)
 
-		return jobId
-
-
-	def getJobState(self, jobId):
-		cmdArray = ['fts-transfer-status', '-s', config.Fts3Endpoint, jobId]
-		logging.debug("Spawning %s" % ' '.join(cmdArray))
-		proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-		rcode = proc.wait()
-		if rcode != 0:
-			logging.error(proc.stdout.read())
-			logging.error(proc.stderr.read())
-			raise Exception("fts-transfer-status failed with exit code %d" % rcode)
-		state = proc.stdout.read().strip()
-		return state
+        return jobId
 
 
-	def poll(self, jobId):
-		state = self.getJobState(jobId)
-		remaining = config.Timeout
-		while state not in fts3.JobTerminalStates:
-			logging.debug("%s %s" % (jobId, state))
-			time.sleep(config.PollInterval)
-			remaining -= config.PollInterval
-			state = self.getJobState(jobId)
-			if remaining <= 0:
-				logging.error("Timeout expired, cancelling job")
-				self.cancel(jobId)
-				raise Exception("Timeout expired while polling")
-
-		return state
+    def getJobState(self, jobId):
+        cmdArray = ['fts-transfer-status', '-s', config.Fts3Endpoint, jobId]
+        logging.debug("Spawning %s" % ' '.join(cmdArray))
+        proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        rcode = proc.wait()
+        if rcode != 0:
+            logging.error(proc.stdout.read())
+            logging.error(proc.stderr.read())
+            raise Exception("fts-transfer-status failed with exit code %d" % rcode)
+        state = proc.stdout.read().strip()
+        return state
 
 
-	def cancel(self, jobId):
-		cmdArray = ['fts-transfer-cancel', '-s', config.Fts3Endpoint, jobId]
-		logging.debug("Spawning %s" % ' '.join(cmdArray))
-		proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-		rcode = proc.wait()
-		if rcode != 0:
-			logging.error(proc.stdout.read())
-			logging.error(proc.stderr.read())
-			raise Exception("fts-transfer-cancel failed with exit code %d" % rcode)
+    def poll(self, jobId):
+        state = self.getJobState(jobId)
+        remaining = config.Timeout
+        while state not in fts3.JobTerminalStates:
+            logging.debug("%s %s" % (jobId, state))
+            time.sleep(config.PollInterval)
+            remaining -= config.PollInterval
+            state = self.getJobState(jobId)
+            if remaining <= 0:
+                logging.error("Timeout expired, cancelling job")
+                self.cancel(jobId)
+                raise Exception("Timeout expired while polling")
+
+        return state
+
+
+    def cancel(self, jobId):
+        cmdArray = ['fts-transfer-cancel', '-s', config.Fts3Endpoint, jobId]
+        logging.debug("Spawning %s" % ' '.join(cmdArray))
+        proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        rcode = proc.wait()
+        if rcode != 0:
+            logging.error(proc.stdout.read())
+            logging.error(proc.stderr.read())
+            raise Exception("fts-transfer-cancel failed with exit code %d" % rcode)
+
+
+    def getFileInfo(self, jobId):
+        cmdArray = ['fts-transfer-status', '-s', config.Fts3Endpoint, 
+                    '--json', '-l', jobId]
+        logging.debug("Spawning %s" % ' '.join(cmdArray))
+        proc = subprocess.Popen(cmdArray, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        rcode = proc.wait()
+        if rcode != 0:
+            logging.error(proc.stdout.read())
+            logging.error(proc.stderr.read())
+            raise Exception("fts-transfer-status failed with exit code %d" % rcode)
+        jsonDetailedState = proc.stdout.read().strip()
+        detailedState = json.loads(jsonDetailedState)
+        fileStates = detailedState['job'][0]['files']
+        pairDict = {}
+        for f in fileStates:
+            src = f['source']
+            dst = f['destination']
+            pairDict[(src,dst)] = f
+        return pairDict
 
