2e60afdbc1fafaa39decf15fc96fb22374cad4ad
Michail Salichos <michail.salichos@cern.ch>
2013-11-04 10:43:37 +0000

    add missing columns to schema patch (for 3.2.0)

--- src/db/schema/t_file_activity_patch.sql
+++ src/db/schema/t_file_activity_patch.sql
@@ -2,5 +2,12 @@
 ALTER TABLE t_file
 ADD activity   VARCHAR(255) DEFAULT "default";
 
+
+ALTER TABLE t_optimize_active
+ADD message   VARCHAR(512);
+
+ALTER TABLE t_optimize_active
+ADD datetime   TIMESTAMP;
+
 -- create the index on activity
 CREATE INDEX t_file_activity ON t_file(activity);
