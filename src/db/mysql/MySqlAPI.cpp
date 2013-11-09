aad642e09df8953fe65ef54486e433f714bf0282
Michail Salichos <michail.salichos@cern.ch>
2013-11-09 10:22:06 +0000

    active must be reduced when success rate goes low than 100% no matter the throughout

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -2595,7 +2595,7 @@ bool MySqlAPI::isTrAllowed(const std::string & /*source_hostname1*/, const std::
                                         active = maxActive - 1;
                                     sql << "update t_optimize_active set active=:active where source_se=:source and dest_se=:dest ",soci::use(active), soci::use(source_hostname), soci::use(destin_hostname);
                                 }
-                            else if (ratioSuccessFailure < 100 && throughput != 0 && avgThr !=0)
+                            else if (ratioSuccessFailure < 100)
                                 {
                                     if(active < highDefault || maxActive < highDefault)
                                         active = highDefault;
