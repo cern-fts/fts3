3bd1d6f19a76895c6276c81c67c79feb8bba513b
Michail Salichos <michail.salichos@cern.ch>
2013-12-04 09:12:38 +0000

    fix transfer duration and an invalid oracle sql statement

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -1116,13 +1116,13 @@ void MySqlAPI::getTransferFileStatus(std::string requestID, bool archive,
             if (archive)
                 {
                     query = "SELECT t_file_backup.file_id, t_file_backup.source_surl, t_file_backup.dest_surl, t_file_backup.file_state, "
-                            "       t_file_backup.reason, t_file_backup.start_time, t_file_backup.finish_time, t_file_backup.retry "
+                            "       t_file_backup.reason, t_file_backup.start_time, t_file_backup.finish_time, t_file_backup.retry, t_file_backup.tx_duration "
                             "FROM t_file_backup WHERE t_file_backup.job_id = :jobId ";
                 }
             else
                 {
                     query = "SELECT t_file.file_id, t_file.source_surl, t_file.dest_surl, t_file.file_state, "
-                            "       t_file.reason, t_file.start_time, t_file.finish_time, t_file.retry "
+                            "       t_file.reason, t_file.start_time, t_file.finish_time, t_file.retry, t_file.tx_duration "
                             "FROM t_file WHERE t_file.job_id = :jobId ";
                 }
 
