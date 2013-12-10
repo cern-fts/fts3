481ea13a595b37ce1fa419fcbef1aef875d1b011
Michail Salichos <michail.salichos@cern.ch>
2013-12-10 13:35:41 +0000

    add SRM_AUTHENTICATION_FAILURE to not recoverable erros

--- src/url-copy/heuristics.cpp
+++ src/url-copy/heuristics.cpp
@@ -90,7 +90,10 @@ bool retryTransfer(int errorNo, const std::string& category, const std::string& 
     found = message.find("SRM_DUPLICATION_ERROR");
     if (found!=std::string::npos)
         retry = false;
-
+    found = message.find("SRM_AUTHENTICATION_FAILURE");
+    if (found!=std::string::npos)
+        retry = false;	
+		
     return retry;
 }
 
