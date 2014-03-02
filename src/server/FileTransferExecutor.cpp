cab96cf60bc8e6e8a6c4de48b8b26693ae47bae5
Michail Salichos <michail.salichos@cern.ch>
2014-03-02 16:31:23 +0000

    disable submitted and ready monitoring messages

--- src/server/FileTransferExecutor.cpp
+++ src/server/FileTransferExecutor.cpp
@@ -127,7 +127,7 @@ int FileTransferExecutor::execute()
                 {
                     scheduled = 1;
 
-                    SingleTrStateInstance::instance().sendStateMessage(tf->JOB_ID, tf->FILE_ID);
+                    //SingleTrStateInstance::instance().sendStateMessage(tf->JOB_ID, tf->FILE_ID);
                     bool isAutoTuned = false;
 
                     if (optimize && cfgs.empty())
--- src/server/FileTransferScheduler.cpp
+++ src/server/FileTransferScheduler.cpp
@@ -128,11 +128,13 @@ bool FileTransferScheduler::schedule(bool optimize)
                             db->setFilesToNotUsed(file->JOB_ID, file->FILE_INDEX, notUsed);
                             if(!notUsed.empty())
                                 {
+				    /*disabled for now but pls do not remove
                                     std::vector<int>::const_iterator iter;
                                     for (iter = notUsed.begin(); iter != notUsed.end(); ++iter)
                                         {
                                             SingleTrStateInstance::instance().sendStateMessage(file->JOB_ID, (*iter));
                                         }
+				    */	
                                     notUsed.clear();
                                 }
                             return true;
@@ -210,11 +212,13 @@ bool FileTransferScheduler::schedule(bool optimize)
             db->setFilesToNotUsed(file->JOB_ID, file->FILE_INDEX, notUsed);
             if(!notUsed.empty())
                 {
+		    /*disabled for now but pls do not remove
                     std::vector<int>::const_iterator iter;
                     for (iter = notUsed.begin(); iter != notUsed.end(); ++iter)
                         {
                             SingleTrStateInstance::instance().sendStateMessage(file->JOB_ID, (*iter));
                         }
+		    */
                     notUsed.clear();
                 }
 
