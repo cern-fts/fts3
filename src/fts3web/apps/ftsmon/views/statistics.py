5b773b22c32eaf48ba88a360c26650a731d22de4
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-05-13 07:26:11 +0000

    Monitoring: Normalize statistics.py

--- src/fts3web/apps/ftsmon/views/statistics.py
+++ src/fts3web/apps/ftsmon/views/statistics.py
@@ -17,29 +17,30 @@
 
 from datetime import datetime, timedelta
 from django.db import connection
-from django.db.models import Q, Count, Avg, Sum
+from django.db.models import Q, Count, Sum
 from django.db.utils import DatabaseError
+
 from ftsweb.models import Job, File, Host
-from ftsweb.models import ProfilingSnapshot, ProfilingInfo, OptimizeActive
-from ftsweb.models import ACTIVE_STATES, FILE_TERMINAL_STATES, STATES
+from ftsweb.models import ProfilingSnapshot, ProfilingInfo
+from ftsweb.models import ACTIVE_STATES, STATES
 from jsonify import jsonify, jsonify_paged
 from util import get_order_by, ordered_field
 import settings
 
 
-def _getCountPerState(age, hostname):
+def _get_count_per_state(age, hostname):
     count = {}
 
-    notBefore = datetime.utcnow() - age
+    not_before = datetime.utcnow() - age
     for state in STATES:
         query = File.objects
         if state in ACTIVE_STATES:
-            query = query.filter(job_finished__isnull = True)
+            query = query.filter(job_finished__isnull=True)
         else:
-            query = query.filter(job_finished__gte = notBefore)
+            query = query.filter(job_finished__gte=not_before)
         if hostname:
-            query = query.filter(transferHost = hostname)
-        query = query.filter(file_state = state)
+            query = query.filter(transferHost=hostname)
+        query = query.filter(file_state=state)
 
         count[state.lower()] = query.count()
 
@@ -50,35 +51,35 @@ def _getCountPerState(age, hostname):
     return count
 
 
-def _getTransferAndSubmissionPerHost(timewindow):
+def _get_transfer_and_submission_per_host(timewindow):
     servers = {}
 
-    notBefore = datetime.utcnow() - timewindow
+    not_before = datetime.utcnow() - timewindow
     hosts = Host.objects.filter().values('hostname').distinct()
 
     for host in map(lambda h: h['hostname'], hosts):
-        submissions = Job.objects.filter(submit_time__gte = notBefore, submit_host = host).count()
-        transfers = File.objects.filter(job_finished__gte = notBefore, transferHost = host).count()
-        actives = File.objects.filter(file_state = 'ACTIVE', transferHost = host).count()
+        submissions = Job.objects.filter(submit_time__gte=not_before, submit_host=host).count()
+        transfers = File.objects.filter(job_finished__gte=not_before, transferHost=host).count()
+        actives = File.objects.filter(file_state='ACTIVE', transferHost=host).count()
         servers[host] = {
-             'submissions': submissions,
-             'transfers': transfers,
-             'active': actives
+            'submissions': submissions,
+            'transfers': transfers,
+            'active': actives
         }
 
     return servers
 
 
-def _getRetriedStats(timewindow, hostname):
-    notBefore = datetime.utcnow() - timewindow
+def _get_retried_stats(timewindow, hostname):
+    not_before = datetime.utcnow() - timewindow
 
-    retriedObjs = File.objects.filter(file_state__in = ['FAILED', 'FINISHED'], job_finished__gte = notBefore, retry__gt = 0)
+    retried_objs = File.objects.filter(file_state__in=['FAILED', 'FINISHED'], job_finished__gte=not_before, retry__gt=0)
     if hostname:
-        retriedObjs = retriedObjs.filter(transferHost = hostname)
-    retriedObjs = retriedObjs.values('file_state').annotate(number = Count('file_state'))
+        retried_objs = retried_objs.filter(transferHost=hostname)
+    retried_objs = retried_objs.values('file_state').annotate(number=Count('file_state'))
 
     retried = {}
-    for f in retriedObjs:
+    for f in retried_objs:
         retried[f['file_state'].lower()] = f['number']
     for s in [s for s in ['failed', 'finished'] if s not in retried]:
         retried[s] = 0
@@ -87,36 +88,35 @@ def _getRetriedStats(timewindow, hostname):
 
 
 @jsonify
-def overview(httpRequest):
-    hostname = httpRequest.GET.get('hostname', None)
+def get_overview(http_request):
+    hostname = http_request.GET.get('hostname', None)
 
-    lastHour = _getCountPerState(timedelta(hours = 1), hostname)
-    retried = _getRetriedStats(timedelta(hours = 1), hostname)
+    last_hour = _get_count_per_state(timedelta(hours=1), hostname)
+    retried = _get_retried_stats(timedelta(hours=1), hostname)
 
     return {
-       'lasthour': lastHour,
-       'retried': retried
+        'lasthour': last_hour,
+        'retried': retried
     }
 
 
-def _getHostServiceAndSegment():
+# noinspection PyTypeChecker
+def _get_host_service_and_segment():
     service_names = map(lambda s: s['service_name'], Host.objects.values('service_name').distinct().all())
 
-    last_expected_beat = datetime.utcnow() - timedelta(minutes = 2)
+    last_expected_beat = datetime.utcnow() - timedelta(minutes=2)
 
     host_map = dict()
     for service in service_names:
-        hosts = Host.objects.filter(service_name = service).values('hostname', 'beat', 'drain').order_by('hostname').all()
+        hosts = Host.objects.filter(service_name=service).values('hostname', 'beat', 'drain').order_by('hostname').all()
         running = filter(lambda h: h['beat'] >= last_expected_beat, hosts)
 
-        host_count = len(hosts)
         running_count = len(running)
 
         if running_count > 0:
             segment_size = 0xFFFF / running_count
             segment_remaining = 0xFFFF % running_count
 
-            segments = dict()
             index = 0
             for host in hosts:
                 hostname = host['hostname']
@@ -128,7 +128,7 @@ def _getHostServiceAndSegment():
                     host_map[hostname][service] = {
                         'status': 'running',
                         'start': "%04X" % (segment_size * index),
-                        'end' : "%04X" % (segment_size * (index + 1) - 1),
+                        'end': "%04X" % (segment_size * (index + 1) - 1),
                         'drain': host['drain'],
                         'beat': host['beat']
                     }
@@ -156,9 +156,9 @@ def _getHostServiceAndSegment():
 
 
 @jsonify
-def servers(httpRequest):
-    segments  = _getHostServiceAndSegment()
-    transfers = _getTransferAndSubmissionPerHost(timedelta(hours = 1))
+def get_servers(http_request):
+    segments = _get_host_service_and_segment()
+    transfers = _get_transfer_and_submission_per_host(timedelta(hours=1))
 
     hosts = segments.keys()
 
@@ -176,34 +176,34 @@ def servers(httpRequest):
 
 
 @jsonify
-def pervo(httpRequest):
-    notBefore = datetime.utcnow() - timedelta(minutes = 30)
+def get_pervo(http_request):
+    not_before = datetime.utcnow() - timedelta(minutes=30)
 
-    query = File.objects.values('file_state', 'vo_name')\
-                        .filter(Q(job_finished__gte = notBefore) | Q(job_finished__isnull = True))\
-                        .annotate(count = Count('file_state'))
+    query = File.objects.values('file_state', 'vo_name') \
+        .filter(Q(job_finished__gte=not_before) | Q(job_finished__isnull=True)) \
+        .annotate(count=Count('file_state'))
 
-    if httpRequest.GET.get('source_se', None):
-        query = query.filter(source_se = httpRequest.GET['source_se'])
-    if httpRequest.GET.get('dest_se', None):
-        query = query.filter(source_se = httpRequest.GET['dest_se'])
+    if http_request.GET.get('source_se', None):
+        query = query.filter(source_se=http_request.GET['source_se'])
+    if http_request.GET.get('dest_se', None):
+        query = query.filter(source_se=http_request.GET['dest_se'])
 
     query = query.order_by('file_state')
 
-    perVo = {}
+    per_vo = {}
     for voJob in query:
         vo = voJob['vo_name']
-        if vo not in perVo:
-            perVo[vo] = {}
-        perVo[vo][voJob['file_state']] = voJob['count']
+        if vo not in per_vo:
+            per_vo[vo] = {}
+        per_vo[vo][voJob['file_state']] = voJob['count']
 
-    return perVo
+    return per_vo
 
 
 class CalculateVolume(object):
-    def __init__(self, triplets, notBefore):
+    def __init__(self, triplets, not_before):
         self.triplets = triplets
-        self.notBefore = notBefore
+        self.not_before = not_before
 
     def __len__(self):
         return len(self.triplets)
@@ -212,37 +212,38 @@ class CalculateVolume(object):
         if not isinstance(indexes, slice):
             indexes = [indexes]
         for triplet in self.triplets[indexes]:
-            pairVolume = File.objects.filter(
-                 source_se = triplet['source_se'],
-                 dest_se = triplet['dest_se'],
-                 vo_name = triplet['vo'],
-                 file_state = 'FINISHED',
-                 job_finished__lt = self.notBefore
-            ).aggregate(vol = Sum('filesize'))
-            triplet['volume'] = pairVolume['vol']
+            pair_volume = File.objects.filter(
+                source_se=triplet['source_se'],
+                dest_se=triplet['dest_se'],
+                vo_name=triplet['vo'],
+                file_state='FINISHED',
+                job_finished__lt=self.not_before
+            ).aggregate(vol=Sum('filesize'))
+            triplet['volume'] = pair_volume['vol']
             yield triplet
 
-@jsonify_paged
-def transferVolume(httpRequest):
-    try:
-        timeWindow = timedelta(hours = int(httpRequest.GET['time_window']))
-    except:
-        timeWindow = timedelta(hours = 1)
-    notBefore = datetime.utcnow() - timeWindow
 
-    if httpRequest.GET.get('vo', None):
-        vos= [httpRequest.GET['vo']]
+@jsonify_paged
+def get_transfer_volume(http_request):
+    try:
+        time_window = timedelta(hours=int(http_request.GET['time_window']))
+    except:
+        time_window = timedelta(hours=1)
+    not_before = datetime.utcnow() - time_window
+
+    if http_request.GET.get('vo', None):
+        vos = [http_request.GET['vo']]
     else:
         vos = [vo['vo_name'] for vo in Job.objects.values('vo_name').distinct().all()]
 
     triplets = []
     for vo in vos:
         pairs = File.objects.values('source_se', 'dest_se').distinct()
-        if httpRequest.GET.get('source_se', None):
-            pairs = pairs.filter(source_se = httpRequest.GET['source_se'])
-        if httpRequest.GET.get('dest_se', None):
-            pairs = pairs.filter(dest_se = httpRequest.GET['dest_se'])
-        pairs = pairs.filter(vo_name = vo, file_state = 'FINISHED', job_finished__lt = notBefore)
+        if http_request.GET.get('source_se', None):
+            pairs = pairs.filter(source_se=http_request.GET['source_se'])
+        if http_request.GET.get('dest_se', None):
+            pairs = pairs.filter(dest_se=http_request.GET['dest_se'])
+        pairs = pairs.filter(vo_name=vo, file_state='FINISHED', job_finished__lt=not_before)
 
         for pair in pairs:
             source = pair['source_se']
@@ -254,37 +255,36 @@ def transferVolume(httpRequest):
             })
 
     # Trick to calculate the sum only for those that are visible
-    return CalculateVolume(triplets, notBefore)
+    return CalculateVolume(triplets, not_before)
 
 
 @jsonify
-def profiling(httpRequest):
+def get_profiling(http_request):
     profiling = {}
 
     info = ProfilingInfo.objects.all()
     if len(info) > 0:
         profiling['updated'] = info[0].updated
-        profiling['period']  = info[0].period
+        profiling['period'] = info[0].period
     else:
         profiling['updated'] = False
-        profiling['period']  = False
-
+        profiling['period'] = False
 
     profiles = ProfilingSnapshot.objects
-    if not httpRequest.GET.get('showall', False):
-        profiles = profiles.filter(cnt__gt = 0)
+    if not http_request.GET.get('showall', False):
+        profiles = profiles.filter(cnt__gt=0)
 
-    (orderBy, orderDesc) = get_order_by(httpRequest)
-    if orderBy == 'scope':
-        profiles = profiles.order_by(ordered_field('scope', orderDesc))
-    elif orderBy == 'called':
-        profiles = profiles.order_by(ordered_field('cnt', orderDesc))
-    elif orderBy == 'aggregate':
-        profiles = profiles.order_by(ordered_field('total', orderDesc))
-    elif orderBy == 'average':
-        profiles = profiles.order_by(ordered_field('average', orderDesc))
-    elif orderBy == 'exceptions':
-        profiles = profiles.order_by(ordered_field('exceptions', orderDesc))
+    (order_by, order_desc) = get_order_by(http_request)
+    if order_by == 'scope':
+        profiles = profiles.order_by(ordered_field('scope', order_desc))
+    elif order_by == 'called':
+        profiles = profiles.order_by(ordered_field('cnt', order_desc))
+    elif order_by == 'aggregate':
+        profiles = profiles.order_by(ordered_field('total', order_desc))
+    elif order_by == 'average':
+        profiles = profiles.order_by(ordered_field('average', order_desc))
+    elif order_by == 'exceptions':
+        profiles = profiles.order_by(ordered_field('exceptions', order_desc))
     else:
         profiles = profiles.order_by('total')
 
@@ -293,35 +293,34 @@ def profiling(httpRequest):
     return profiling
 
 
-def _slowEntry2Dict(queries):
+def _slow_entry_to_dict(queries):
     for q in queries:
         yield {
-           'start_time':     q[0],
-           'user_host':      q[1],
-           'query_time':     q[2],
-           'lock_time':      q[3],
-           'rows_sent':      q[4],
-           'rows_examined':  q[5],
-           'db':             q[6],
-           'last_insert_id': q[7],
-           'insert_id':      q[8],
-           'server_id':      q[9],
-           'sql_text':       q[10]
+            'start_time': q[0],
+            'user_host': q[1],
+            'query_time': q[2],
+            'lock_time': q[3],
+            'rows_sent': q[4],
+            'rows_examined': q[5],
+            'db': q[6],
+            'last_insert_id': q[7],
+            'insert_id': q[8],
+            'server_id': q[9],
+            'sql_text': q[10]
         }
 
 
 @jsonify
-def slowQueries(httpRequest):
+def get_slow_queries(http_request):
     engine = settings.DATABASES['default']['ENGINE']
 
     if engine.endswith('oracle'):
         return {'message': 'Not supported for Oracle'}
 
     try:
-        dbName = settings.DATABASES['default']['NAME']
+        dbname = settings.DATABASES['default']['NAME']
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM mysql.slow_log WHERE db = %s ORDER BY query_time DESC" , (dbName))
-        return {'queries': _slowEntry2Dict(cursor.fetchall())}
-    except DatabaseError, e:
+        cursor.execute("SELECT * FROM mysql.slow_log WHERE db = %s ORDER BY query_time DESC", [dbname])
+        return {'queries': _slow_entry_to_dict(cursor.fetchall())}
+    except DatabaseError:
         return {'message': 'Could not execute the query'}
-
