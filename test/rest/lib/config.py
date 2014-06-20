def41ccb8859fb64f60613b1746163e700d9cfd4
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-06-20 08:40:38 +0000

    Tests: Disabled http tests (rfc proxy disabled as voms-clients is broken)

--- test/rest/lib/config.py
+++ test/rest/lib/config.py
@@ -41,10 +41,10 @@ StorageAreaPairs = [
         'srm://grid002.ft.uam.es:8443/srm/managerv2?SFN=/pnfs/ft.uam.es/data/%(vo)s/',
         'srm://storage01.lcg.cscs.ch:8443/srm/managerv2?SFN=/pnfs/lcg.cscs.ch/%(vo)s/'
     ),
-    (
-        'davs://lxfsra04a04.cern.ch/dpm/cern.ch/home/%(vo)s/',
-        'davs://lxfsra10a01.cern.ch/dpm/cern.ch/home/%(vo)s/'
-    )
+#    (
+#        'davs://lxfsra04a04.cern.ch/dpm/cern.ch/home/%(vo)s/',
+#        'davs://lxfsra10a01.cern.ch/dpm/cern.ch/home/%(vo)s/'
+#    )
 ]
 
 # Logging level
