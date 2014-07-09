ac13dd991e7973ca9197dc33b031fb649d8ce981
Michal Kamil Simon <michal.simon@cern.ch>
2014-07-09 12:34:47 +0000

    #1084 	implement daemon site bring-online cancel

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -10703,6 +10703,9 @@ void MySqlAPI::getStagingFilesForCanceling(std::vector< boost::tuple<int, std::s
                                           "  AND (hashed_id >= :hStart AND hashed_id <= :hEnd)  AND staging_start is NOT NULL ",
                                           soci::use(hashSegment.start), soci::use(hashSegment.end));
 
+            soci::statement stmt1 = (sql.prepare << "UPDATE t_file SET  job_finished = UTC_TIMESTAMP() "
+                                     "WHERE file_id = :file_id ", soci::use(file_id, "file_id"));
+
             // Cancel staging files
             for (soci::rowset<soci::row>::const_iterator i2 = rs.begin(); i2 != rs.end(); ++i2)
                 {
@@ -10712,6 +10715,8 @@ void MySqlAPI::getStagingFilesForCanceling(std::vector< boost::tuple<int, std::s
                     token = row.get<std::string>("bringonline_token","");
                     boost::tuple<int, std::string, std::string> record(file_id, source_surl, token);
                     files.push_back(record);
+
+                    stmt1.execute(true);
                 }
         }
     catch (std::exception& e)
