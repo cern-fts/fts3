b209b95b2d485e37bd6a145f171d24ddf377ff7a
Michail Salichos <michail.salichos@cern.ch>
2014-08-18 04:49:39 +0000

    show exception message

--- src/server/cleanStateLogs.h
+++ src/server/cleanStateLogs.h
@@ -149,6 +149,15 @@ private:
                                 counter = 0;
                             }
                     }
+   		catch(std::exception& e)
+        	     {
+                        sleep(1); 
+                        if(waitTime == 86400) //reset
+                            waitTime = 0;
+
+                        counter = 0;		
+            		FTS3_COMMON_LOGGER_NEWLOG(ERR) << "Cannot delete old files " << e.what() <<  commit;
+        	     }		    
                 catch(...)
                     {
                         sleep(1); //once a day
