a7649f5a862f9eb55e842777b11b347ca431554f
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-10-21 15:31:36 +0000

    Fixed a typo in FileTransferStatus conversion

--- src/common/version.h
+++ src/common/version.h
@@ -1,1 +1,1 @@
-#define VERSION "3.2.29"
+#define VERSION "3.2.30"
--- src/db/oracle/sociConversions.h
+++ src/db/oracle/sociConversions.h
@@ -302,14 +302,8 @@ struct type_conversion<FileTransferStatus>
     {
         transfer.fileId            = static_cast<int>(v.get<long long>("FILE_ID"));
         transfer.sourceSURL        = v.get<std::string>("SOURCE_SURL");
-        try
-            {
-                transfer.destSURL          = v.get<std::string>("DEST_SURL");
-            }
-        catch(...)
-            {
-                // ignore since DM operations (deletion) do not have destination
-            }
+        transfer.destSURL          = v.get<std::string>("DEST_SURL", "");
+
         transfer.transferFileState = v.get<std::string>("FILE_STATE");
         transfer.reason            = v.get<std::string>("REASON", "");
         transfer.numFailures	   = static_cast<int>(v.get<double>("RETRY", 0));
