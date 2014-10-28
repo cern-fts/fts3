8a4b103f24541c0cf5fa0dd74abded5ed4b3cb87
Michail Salichos <michail.salichos@cern.ch>
2014-10-28 20:53:18 +0000

    order by file_id

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -978,9 +978,9 @@ void MySqlAPI::getByJobId(std::vector< boost::tuple<std::string, std::string, st
                                                               "     f.vo_name = :vo_name AND     f.wait_timestamp IS NULL AND "
                                                               "     (f.retry_timestamp is NULL OR f.retry_timestamp < :tTime) AND "
                                                               "     (j.reuse_job = 'N' OR j.reuse_job = 'R') AND "
-                                                              "     (f.hashed_id >= :hStart AND f.hashed_id <= :hEnd) and  f.vo_name= j.vo_name and j.job_finished is null and "
+                                                              "     (f.hashed_id >= :hStart AND f.hashed_id <= :hEnd) and  f.vo_name= j.vo_name and f.job_finished is null and "
                                                               " exists (select * from t_job y where y.job_id=j.job_id  "
-                                                              " ORDER BY y.priority DESC, y.submit_time) LIMIT :filesNum",
+                                                              " ORDER BY y.priority DESC, y.submit_time) order by f.file_id LIMIT :filesNum",
                                                               soci::use(boost::get<0>(triplet)),
                                                               soci::use(boost::get<1>(triplet)),
                                                               soci::use(boost::get<2>(triplet)),
@@ -1048,8 +1048,8 @@ void MySqlAPI::getByJobId(std::vector< boost::tuple<std::string, std::string, st
                                         :
                                         "	  f.activity = :activity AND ";
                                     select +=
-                                        "    (f.hashed_id >= :hStart AND f.hashed_id <= :hEnd) and f.vo_name= j.vo_name and j.job_finished is null and exists (select * from t_job y where y.job_id=j.job_id  "
-                                        " ORDER BY y.priority DESC, y.submit_time) LIMIT :filesNum";
+                                        "    (f.hashed_id >= :hStart AND f.hashed_id <= :hEnd) and f.vo_name= j.vo_name and f.job_finished is null and exists (select * from t_job y where y.job_id=j.job_id  "
+                                        " ORDER BY y.priority DESC, y.submit_time) order by f.file_id LIMIT :filesNum";
 
 
                                     soci::rowset<TransferFiles> rs = (
