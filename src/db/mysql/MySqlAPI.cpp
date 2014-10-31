9f8653202d386bb93fa8734cbe3c103800b33d21
Michail Salichos <michail.salichos@cern.ch>
2014-10-31 05:23:58 +0000

    add order by file_id

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -983,7 +983,7 @@ void MySqlAPI::getByJobId(std::vector< boost::tuple<std::string, std::string, st
                                                               "     (j.reuse_job = 'N' OR j.reuse_job = 'R') AND "
                                                               "     (f.hashed_id >= :hStart AND f.hashed_id <= :hEnd) and "
                                                               " exists (select * from t_job y where y.job_id=j.job_id  "
-                                                              " ORDER BY y.priority DESC, y.submit_time) LIMIT :filesNum",
+                                                              " ORDER BY y.priority DESC, y.submit_time) order by f.file_id LIMIT :filesNum",
                                                               soci::use(boost::get<0>(triplet)),
                                                               soci::use(boost::get<1>(triplet)),
                                                               soci::use(boost::get<2>(triplet)),
@@ -1052,7 +1052,7 @@ void MySqlAPI::getByJobId(std::vector< boost::tuple<std::string, std::string, st
                                         "	  f.activity = :activity AND ";
                                     select +=
                                         "    (f.hashed_id >= :hStart AND f.hashed_id <= :hEnd) and exists (select * from t_job y where y.job_id=j.job_id  "
-                                        " ORDER BY y.priority DESC, y.submit_time) LIMIT :filesNum";
+                                        " ORDER BY y.priority DESC, y.submit_time) order by f.file_id LIMIT :filesNum";
 
 
                                     soci::rowset<TransferFiles> rs = (
