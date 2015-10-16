f7a9d46aa7249c7f0f52b5013f9c37dfcf052b36
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-10-16 16:13:48 +0200

    Small refactors in clean.cpp

--- src/dbClear/clean.cpp
+++ src/dbClear/clean.cpp
@@ -18,19 +18,12 @@
  * limitations under the License.
  */
 
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-#include <cstdio>
 #include <iostream>
 #include <boost/filesystem.hpp>
 
-#include "../config/ServerConfig.h"
 #include "common/Exceptions.h"
 #include "common/Logger.h"
+#include "config/ServerConfig.h"
 #include "db/generic/SingleDbInstance.h"
 #include "profiler/Profiler.h"
 
@@ -40,85 +33,65 @@ using namespace fts3::config;
 using namespace fts3::common;
 using namespace db;
 
-/* -------------------------------------------------------------------------- */
-void fts3_initialize_db_backend()
+
+static void initializeDbBackend()
 {
     std::string dbUserName = ServerConfig::instance().get<std::string>("DbUserName");
     std::string dbPassword = ServerConfig::instance().get<std::string>("DbPassword");
     std::string dbConnectString = ServerConfig::instance().get<std::string>("DbConnectString");
 
-    try
-        {
-            db::DBSingleton::instance().getDBObjectInstance()->init(dbUserName, dbPassword, dbConnectString, 1);
-        }
-    catch(UserError& e)
-        {
-            FTS3_COMMON_LOGGER_NEWLOG(ERR) << e.what() << commit;
-            exit(1);
-        }
-    catch(...)
-        {
-            FTS3_COMMON_LOGGER_NEWLOG(ERR) << "Something is going on with the database, check username/password/connstring" << commit;
-            exit(1);
-        }
+    db::DBSingleton::instance().getDBObjectInstance()->init(dbUserName, dbPassword, dbConnectString, 1);
 }
 
 
 int main(int argc, char** argv)
 {
+    try {
+        const char *configfile = "/etc/fts3/fts3config";
 
-    try
-        {
-            const char *configfile = "/etc/fts3/fts3config";
-
-            if (!fs::exists(configfile))
-                {
-                    std::cerr << "fts3 server config file doesn't exist"  << std::endl;
-                    exit(1);
-                }
-
-            ServerConfig::instance().read(argc, argv);
-
-            std::string logDir = ServerConfig::instance().get<std::string > ("ServerLogDirectory");
-            if (logDir.length() > 0)
-                {
-                    logDir += "/fts3server.log";
-                    if (theLogger().redirect(logDir, logDir) < 0)
-                        {
-                            std::cerr << "fts3 server failed to open log file, errno is:" << strerror(errno) << std::endl;
-                            exit(1);
-                        }
-                }
-
-
-            fts3_initialize_db_backend();
-
-            long bulkSize = ServerConfig::instance().get<long>("CleanBulkSize");
-
-            FTS3_COMMON_LOGGER_NEWLOG(INFO)<< "Backup starting with bulk size of " << bulkSize << commit;
-            long nJobs = 0, nFiles = 0;
-
-            db::DBSingleton::instance().getDBObjectInstance()->backup(bulkSize, &nJobs, &nFiles);
-
-            FTS3_COMMON_LOGGER_NEWLOG(INFO)<< "Backup ending: "
-                                           << nJobs << " jobs and "
-                                           << nFiles << " files affected"
-                                           << commit;
-
-            // If profiling is configured, dump the timing
-            db::DBSingleton::instance().getDBObjectInstance()->storeProfiling(&fts3::ProfilingSubsystem::instance());
-
+        if (!fs::exists(configfile)) {
+            std::cerr << "fts3 server config file doesn't exist" << std::endl;
+            exit(1);
         }
-    catch (const std::exception& e)
-        {
-            FTS3_COMMON_LOGGER_NEWLOG(ERR) << "Backup fatal error, exiting... " << e.what() << commit;
-            return EXIT_FAILURE;
+
+        ServerConfig::instance().read(argc, argv);
+
+        std::string logPath = ServerConfig::instance().get<std::string>("ServerLogDirectory");
+        if (logPath.length() > 0) {
+            logPath += "/fts3server.log";
+            if (theLogger().redirect(logPath, logPath) < 0) {
+                throw SystemError("Database cleaner failed to open the log file");
+            }
         }
+
+        initializeDbBackend();
+
+        long bulkSize = ServerConfig::instance().get<long>("CleanBulkSize");
+
+        FTS3_COMMON_LOGGER_NEWLOG(INFO)<< "Backup starting with bulk size of " << bulkSize << commit;
+
+        long nJobs = 0, nFiles = 0;
+        db::DBSingleton::instance().getDBObjectInstance()->backup(bulkSize, &nJobs, &nFiles);
+
+        FTS3_COMMON_LOGGER_NEWLOG(INFO)<< "Backup ending: "
+            << nJobs << " jobs and "
+            << nFiles << " files affected"
+            << commit;
+
+        // If profiling is configured, dump the timing
+        db::DBSingleton::instance().getDBObjectInstance()->storeProfiling(
+            &fts3::ProfilingSubsystem::instance());
+
+    }
+    catch (const std::exception& e) {
+        FTS3_COMMON_LOGGER_NEWLOG(ERR)<< "Backup fatal error, exiting... " << e.what() << commit;
+        return EXIT_FAILURE;
+    }
     catch (...)
-        {
-            FTS3_COMMON_LOGGER_NEWLOG(ERR) << "Backup fatal error (unknown origin), exiting..." << commit;
-            return EXIT_FAILURE;
-        }
+    {
+        FTS3_COMMON_LOGGER_NEWLOG(ERR) << "Backup fatal error (unknown origin), exiting..." << commit;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
