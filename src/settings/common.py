4b788b7300d2c320d1fbf7bdefb77c8fa5555534
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-10-29 10:40:40 +0100

    Update license and copyright notices

--- src/settings/common.py
+++ src/settings/common.py
@@ -1,7 +1,9 @@
 # Copyright notice:
-# Copyright (C) Members of the EMI Collaboration, 2010.
 #
-# See www.eu-emi.eu for details on the copyright holders
+# Copyright (C) CERN 2013-2015
+#
+# Copyright (C) Members of the EMI Collaboration, 2010-2013.
+#   See www.eu-emi.eu for details on the copyright holders
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 import os
 import re
