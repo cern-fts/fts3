5b49b06d5694d7acc82efd89ade44647e0133183
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-12-03 15:08:29 +0100

    Remove old scripts

--- compile_debug.sh
+++
@@ -1,5 +0,0 @@
-/usr/bin/cmake28 -DALLBUILD=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr  -DLIB_SUFFIX=64 -DCMAKE_SKIP_RPATH:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=ON  $1
-make 2>&1 | sed -e 's%^.*: error: .*$%\x1b[37;41m&\x1b[m%' -e 's%^.*: warning: .*$%\x1b[30;43m&\x1b[m%' 
-
-
-
--- compile_release.sh
+++
@@ -1,6 +0,0 @@
-rm -fr build
-mkdir build
-cd build
-cmake28 -DBoost_NO_BOOST_CMAKE=ON -DALLBUILD=ON -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_INSTALL_PREFIX='' ../
-make 2>&1 | sed -e 's%^.*: error: .*$%\x1b[37;41m&\x1b[m%' -e 's%^.*: warning: .*$%\x1b[30;43m&\x1b[m%'
-cd ../
