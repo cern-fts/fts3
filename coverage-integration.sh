3366315b6d4db11898e2ecb1e042ba68291ac554
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-12-03 15:05:01 +0100

    Scripting integration coverage

---
+++ coverage-integration.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Extract coverage data out of the functional tests
+
+if [ "$#" -ne 5 ]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+SOURCE_DIR=$1
+BUILD_DIR=$2
+OUTPUT_DIR=$3
+FTS3CONFIG=$4
+MSGCONFIG=$5
+
+
+set -x
+pushd "${BUILD_DIR}"
+
+# Clean coverage data
+lcov --directory . --zerocounters
+
+# Spawn fts_server, fts_bringonline and msg-bulk
+export LD_LIBRARY_PATH="${BUILD_DIR}/src/db/mysql"
+export PATH="$PATH:${BUILD_DIR}/src/url-copy"
+
+./src/server/fts_server -r -f "${FTS3CONFIG}"
+./src/bringonline-daemon/fts_bringonline -r -f "${FTS3CONFIG}"
+./src/monitoring/fts_msg_bulk -f "${FTS3CONFIG}"  "--MonitoringConfigFile=${MSGCONFIG}"
+
+sleep 2
+
+# Get pids
+FTS_PID=`pidof fts_server`
+BRINGONLINE_PID=`pidof fts_bringonline`
+MSG_PID=`pidof fts_msg_bulk`
+
+echo "Running fts_server: ${FTS_PID}"
+echo "Running fts_bringonline: ${BRINGONLINE_PID}"
+echo "Running fts_msg_bulk: ${MSG_PID}"
+
+# Get the tests and run them against the REST API connected to the same DB
+# We assume there is a valid proxy in the environment!
+if [ ! -d "fts-tests" ]; then
+    git clone https://gitlab.cern.ch/fts/fts-tests.git
+fi
+pushd fts-tests
+    ./run-helper.sh
+popd
+
+# Stop the servers
+echo "Stop services..."
+kill "${FTS_PID}"
+kill "${BRINGONLINE_PID}"
+kill "${MSG_PID}"
+sleep 5
+
+kill -9 "${FTS_PID}"
+kill -9 "${BRINGONLINE_PID}"
+kill -9 "${MSG_PID}"
+
+# Generate the coverage lcov data
+lcov --directory . --capture --output-file="${OUTPUT_DIR}/coverage-integration.info"
+
+# Download the converter
+if [ ! -f "lcov_cobertura.py" ]; then
+    wget "https://raw.github.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py"
+fi
+
+# Generate the xml
+python lcov_cobertura.py "${OUTPUT_DIR}/coverage-integration.info" -b "${SOURCE_DIR}" -e ".+usr.include." -o "${OUTPUT_DIR}/coverage-integration.xml"
+
+# Done
+popd
+echo "Done. Functional test coverage in coverage-functional.xml"
--- coverage-unit.sh
+++ coverage-unit.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 # Extract coverage data out of the unit tests
-set -x
-
-if [ "$#" -eq 1 ]; then
-    BUILD_DIR=$1
-else
-    BUILD_DIR="build"
+if [ "$#" -ne 3 ]; then
+    echo "Wrong number of arguments"
+    exit 1
 fi
 
-SOURCE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-RUNNING_DIR=`pwd`
+SOURCE_DIR=$1
+BUILD_DIR=$2
+OUTPUT_DIR=$3
 
-mkdir -p "${BUILD_DIR}"
+set -x
+
 pushd "${BUILD_DIR}"
-
-# Build sources
-${SOURCE_DIR}/coverage-build.sh "${SOURCE_DIR}"
 
 # Clean coverage data
 lcov --directory . --zerocounters
 
 # Run tests
-./test/unit/unit --log_level=all --log_format=XML --report_level=detailed > "${RUNNING_DIR}/tests.xml"
+./test/unit/unit --log_level=all --log_format=XML --report_level=detailed > "${OUTPUT_DIR}/tests.xml"
 
 # Generate the coverage lcov data
-lcov --directory . --capture --output-file="${RUNNING_DIR}/coverage-unit.info"
+lcov --directory . --capture --output-file="${OUTPUT_DIR}/coverage-unit.info"
 
 # Download the converter
 if [ ! -f "lcov_cobertura.py" ]; then
@@ -32,7 +28,7 @@ if [ ! -f "lcov_cobertura.py" ]; then
 fi
 
 # Generate the xml
-python lcov_cobertura.py "${RUNNING_DIR}/coverage-unit.info" -b "${SOURCE_DIR}" -e ".+usr.include." -o "${RUNNING_DIR}/coverage-unit.xml"
+python lcov_cobertura.py "${OUTPUT_DIR}/coverage-unit.info" -b "${SOURCE_DIR}" -e ".+usr.include." -o "${OUTPUT_DIR}/coverage-unit.xml"
 
 # Done!
 popd
