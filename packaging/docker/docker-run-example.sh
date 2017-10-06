c8d38d6f3a4beed6543e7cf15410c860a5733562
Andrea Manzi <andrea.manzi@gmail.com>
2017-10-06 10:50:58 +0200

    fix example

--- packaging/docker/docker-run-example.sh
+++ packaging/docker/docker-run-example.sh
@@ -1,1 +1,1 @@
-docker run -d -p 2170:2170 -v /etc/fts3:/etc/fts3   gitlab-registry.cern.ch/fts/fts3:latest
+docker run -d -p 2170:2170 -v /etc/fts3:/etc/fts3  -v /etc/grid-security:/etc/grid-security  gitlab-registry.cern.ch/fts/fts3:latest
