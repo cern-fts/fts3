393ff7a149788ba62624de70d158487da871ef4f
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2018-01-25 09:55:29 +0100

    Fix bootstrap so fts3 can be run as vagrant user

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,12 @@ yum groupinstall -y 'Development Tools'
 yum install -y epel-release yum-builddep git
 yum-builddep -y "/vagrant/packaging/rpm/fts.spec"
 yum install -y gfal2-all
+
+# Prepare the environment
+mkdir -p "/etc/fts3"
+cp "/vagrant/src/config/fts3config" "/etc/fts3/fts3config"
+
+mkdir -p "/var/lib/fts3/"
+mkdir -p "/var/log/fts3"
+chown vagrant.vagrant "/var/lib/fts3/"
+chown vagrant.vagrant "/var/log/fts3/"
