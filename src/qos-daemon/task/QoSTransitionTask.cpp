aa128f17172cf24af6df8468cd1ca5464cb8eb51
Aris Angelogiannopoulos <aris.angelogiannopoulos@cern.ch>
2018-08-02 16:33:08 +0200

    Create QoSTransitionTask

--- src/qos-daemon/task/QoSTransitionTask.cpp
+++ src/qos-daemon/task/QoSTransitionTask.cpp
@@ -34,33 +34,26 @@ void QoSTransitionTask::run(const boost::any &)
 {
 	std::cout << "Inside the QoS transition task" << std::endl;
 	std::cerr << "This is the set size: " << active_surls.size() << std::endl;
+	std::vector<GError*> errors(active_surls.size(), NULL);
 	for (auto it = active_surls.begin(); it != active_surls.end(); ++it) {
 	      std::cerr << "Printing surl: " << std::get<0>(*it) << std::endl;
+	      std::cerr << "Printing token: " << std::get<1>(*it) << std::endl;
+	      std::cerr << "Printing target_qos: " << std::get<2>(*it) << std::endl;
+
+	      GError *err = NULL;
+
+	      // Add token to context
+	      gfal2_cred_t *cred = gfal2_cred_new(GFAL_CRED_BEARER, std::get<1>(*it).c_str());
+	      std::cerr << "The host is:" << Uri::parse(std::get<0>(*it).c_str()).host << std::endl;
+	      gfal2_cred_set(gfal2_ctx, Uri::parse(std::get<0>(*it)).host.c_str(), cred, &err);
+	      // Perform transition
+	      std::cerr << "About to perform the transition" << std::endl;
+	      //this cause SEGFAULT when the return code is 201 successfull
+	      // need to add error checking
+		  int result = gfal2_change_object_qos(gfal2_ctx, std::get<0>(*it).c_str(), std::get<2>(*it).c_str(), &err);
+		  std::cerr << "This just returned: " << result << std::endl;
+	      //Delete token from context
+		  gfal2_cred_free(cred);
+		  gfal2_cred_clean(gfal2_ctx, &err);
 	}
-    /*char token[512] = {0};
-    std::set<std::string> urlSet = ctx.getUrls();
-    if (urlSet.empty())
-        return;
-
-    std::vector<const char*> urls;
-    urls.reserve(urlSet.size());
-    for (auto set_i = urlSet.begin(); set_i != urlSet.end(); ++set_i) {
-        urls.push_back(set_i->c_str());
-    }
-
-    std::vector<GError*> errors(urls.size(), NULL);
-
-    /////////////FTS3_COMMON_LOGGER_NEWLOG(INFO) << "BRINGONLINE issuing bring-online for: " << urls.size() << " files, "
-    /////////////                                << "with copy-pin-lifetime: " << ctx.getPinlifetime() <<
-    /////////////                                " and bring-online-timeout: " << ctx.getBringonlineTimeout() << commit;
-
-    GError *err = NULL;
-    for (size_t i = 0; i < urls.size(); ++i) {
-    	int result = gfal2_change_object_qos(context, urls[i], "/cdmi_capabilities/dataobject/tape", &err);
-    }
-    g_clear_error(&err);*/
-
-    //for (size_t i = 0; i < urls.size(); ++i) {
-    //    g_clear_error(&errors[i]);
-    //}
 }
