3cd8257ee28df16ce786e7e8269ed817a591c3ac
Andrea Manzi <amanzi@cern.ch>
2019-02-20 13:30:16 +0100

    FTS-1382: trying to fix duplicate url-copy issue

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -1204,9 +1204,13 @@ boost::tuple<bool, std::string>  MySqlAPI::updateFileTransferStatusInternal(soci
         }
 
         // If the file already in the same state, don't do anything either
+        // avoid 2 url-copy on the same file id to start ( condition processId==0)
         if (storedState == newFileState) {
-            sql.rollback();
-            return boost::tuple<bool, std::string>(false, storedState);
+            if (newFileState == "READY" && processId != 0) {}
+	    else {
+                sql.rollback();
+                return boost::tuple<bool, std::string>(false, storedState);
+            }
         }
 
         soci::statement stmt(sql);
