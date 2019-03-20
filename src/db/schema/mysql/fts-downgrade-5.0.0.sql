68b4d9c844f0d71eb3d6249516716b644ed4a64a
Edward Karavakis <edward.karavakis@cern.ch>
2019-03-20 15:18:48 +0100

    Fix downgrade script, do not remove the reason but undo the utf8 encoding and revert to latin1

--- src/db/schema/mysql/fts-downgrade-5.0.0.sql
+++ src/db/schema/mysql/fts-downgrade-5.0.0.sql
@@ -10,21 +10,20 @@ ALTER TABLE `t_file`
 	DROP INDEX `idx_host`;
 ALTER TABLE `t_optimizer_evolution` 
 	DROP INDEX `idx_datetime`;
-ALTER TABLE `t_file` 
-	DROP reason;
-ALTER TABLE `t_file_backup` 
-        DROP reason;
+ALTER TABLE `t_file`
+        MODIFY reason varchar(2048) CHARACTER SET latin1;
+ALTER TABLE `t_file_backup`
+        MODIFY reason varchar(2048) CHARACTER SET latin1;
 ALTER TABLE `t_dm`
-        DROP reason;
+        MODIFY reason varchar(2048) CHARACTER SET latin1;
 ALTER TABLE `t_dm_backup`
-        DROP reason;
+        MODIFY reason varchar(2048) CHARACTER SET latin1;
 ALTER TABLE `t_file_retry_errors`
-        DROP reason;
+        MODIFY reason varchar(2048) CHARACTER SET latin1;
 ALTER TABLE `t_file_retry_errors`
         DROP INDEX `idx_datetime`;
 ALTER TABLE `t_file` 
 	DROP `priority`;
-
 ALTER TABLE `t_file` 
        DROP `dest_surl_uuid`;
 ALTER TABLE `t_file` 
