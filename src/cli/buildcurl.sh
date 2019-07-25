29dded8aed6330c2abae298f7cbe4439bfb77907
Andrea Manzi <amanzi@cern.ch>
2019-07-25 13:48:56 +0200

    FTS-1453: include curl in the source tree as we don't have internet access when builing in fedora

--- src/cli/buildcurl.sh
+++ src/cli/buildcurl.sh
@@ -2,9 +2,7 @@
 # - pwd is the top source directory
 
 
-mkdir -p  ./src/cli/external
 cd ./src/cli/external
-wget https://curl.haxx.se/download/curl-7.65.2.tar.gz
 tar zxvf curl-7.65.2.tar.gz
 mv curl-7.65.2 curl
 cd curl
