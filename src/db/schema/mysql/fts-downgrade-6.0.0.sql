870579cbb4b2b8cb78c392c0bf2bbf2409e8b025
Edward Karavakis <edward.karavakis@cern.ch>
2020-06-22 09:36:13 +0000

    Update fts-downgrade-6.0.0.sql to include undoing the ARCHIVING monitoring changes

--- src/db/schema/mysql/fts-downgrade-6.0.0.sql
+++ src/db/schema/mysql/fts-downgrade-6.0.0.sql
@@ -1,19 +1,29 @@
 --
--- Script to downgrade from FTS3 Schema 6.0.0 to the previous schema
+-- Script to downgrade from FTS3 Schema 6.0.0 to the previous schema, 5.0.0
 -- 
 
 ALTER TABLE `t_job` 
-	DROP COLUMN `target_qos`;
+	DROP COLUMN `target_qos`,
+	DROP COLUMN `archive_timeout`;
 ALTER TABLE `t_job` 
 	MODIFY COLUMN job_state enum('STAGING','SUBMITTED','READY','ACTIVE','FINISHED','FAILED','FINISHEDDIRTY','CANCELED','DELETE') NOT NULL;
 
 ALTER TABLE `t_job_backup` 
 	MODIFY COLUMN job_state enum('STAGING','SUBMITTED','READY','ACTIVE','FINISHED','FAILED','FINISHEDDIRTY','CANCELED','DELETE') NOT NULL;
 ALTER TABLE `t_job_backup` 
-	DROP COLUMN `target_qos`;
+	DROP COLUMN `target_qos`,
+	DROP COLUMN `archive_timeout`;
 
+
+ALTER TABLE `t_file`
+    DROP COLUMN `archive_start_time`,
+    DROP COLUMN `archive_finish_time`;
 ALTER TABLE `t_file` 
 	MODIFY COLUMN file_state enum('STAGING','STARTED','SUBMITTED','READY','ACTIVE','FINISHED','FAILED','CANCELED','NOT_USED','ON_HOLD','ON_HOLD_STAGING') NOT NULL;
+
+ALTER TABLE `t_file_backup`
+    DROP COLUMN `archive_start_time`,
+    DROP COLUMN `archive_finish_time`;
 ALTER TABLE `t_file_backup` 
 	MODIFY COLUMN file_state enum('STAGING','STARTED','SUBMITTED','READY','ACTIVE','FINISHED','FAILED','CANCELED','NOT_USED','ON_HOLD','ON_HOLD_STAGING') NOT NULL;
 
