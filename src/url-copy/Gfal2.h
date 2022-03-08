791f50157f05bc1718815cc64e56da1e2bc6a370
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-03-08 12:01:09 +0100

    Implement method to retrieve Gfal2 config value

--- src/url-copy/Gfal2.h
+++ src/url-copy/Gfal2.h
@@ -347,7 +347,6 @@ public:
         gfal2_cancel(context);
     }
 
-
     /// Stat a file
     struct stat stat(Gfal2TransferParams &params, const std::string &url, bool is_source) {
         bearerInit(params, is_source ? url : "",
@@ -395,7 +394,7 @@ public:
         return buffer;
     }
 
-    // Get the extended attribute of a resource
+    /// Get the extended attribute of a resource
     std::string getXattr(const std::string &url, const std::string &name) {
         char buffer[1024];
         GError *error = NULL;
@@ -403,6 +402,17 @@ public:
             throw Gfal2Exception(error);
         }
         return buffer;
+    }
+
+    /// Get a string config value
+    std::string get(const std::string &group, const std::string &key) {
+        GError *error = NULL;
+        char* value = gfal2_get_opt_string(context, group.c_str(), key.c_str(), &error);
+        if (error != NULL) {
+            value = "N/A";
+            g_error_free(error);
+        }
+        return std::string(value);
     }
 };
 
