b33dc26cab61f841e1d63506b6c823098813d0b3
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-04-28 14:33:44 +0200

    FTS-1786: Add gridmap table

--- src/db/schema/mysql/fts-diff-8.0.0.sql
+++ src/db/schema/mysql/fts-diff-8.0.0.sql
@@ -2,6 +2,7 @@
 -- FTS3 Schema 8.0.0
 -- [FTS-1744] Schema changes for the eviction feature
 -- [FTS-1768] SRM TURL protocol configurable per link
+-- [FTS01786] Add t_gripdmap table
 --
 
 ALTER TABLE `t_se`
@@ -9,6 +10,12 @@ ALTER TABLE `t_se`
 
 ALTER TABLE `t_link_config`
     ADD COLUMN `3rd_party_turl` varchar(150) DEFAULT NULL;
+
+CREATE TABLE `t_gridmap` (
+    `dn` VARCHAR(255) NOT NULL,
+    `vo` VARCHAR(100) NOT NULL,
+    PRIMARY KEY(`dn`)
+);
 
 INSERT INTO t_schema_vers (major, minor, patch, message)
 VALUES (8, 0, 0, 'FTS-1702: Schema changes for the eviction feature');
