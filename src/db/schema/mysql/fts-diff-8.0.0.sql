59c3ae561156b72a9c357f705eef4f1ae74fcb10
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-06-17 12:03:28 +0200

    FTS-1806: Further schema 8.0.0 changes

--- src/db/schema/mysql/fts-diff-8.0.0.sql
+++ src/db/schema/mysql/fts-diff-8.0.0.sql
@@ -1,8 +1,9 @@
 --
 -- FTS3 Schema 8.0.0
 -- [FTS-1744] Schema changes for the eviction feature
+-- [FTS-1759] Add primary key on t_schema_vers table
 -- [FTS-1768] SRM TURL protocol configurable per link
--- [FTS01786] Add t_gripdmap table
+-- [FTS-1786] Add "t_gridmap" table
 -- [FTS-1804] Add "staging_metadata" field to "t_file" table
 -- [FTS-1805] Remove "v_staging" view
 --
@@ -27,5 +28,19 @@ ALTER TABLE `t_file`
 
 DROP VIEW IF EXISTS `v_staging`;
 
+--
+-- Cleanup
+--
+
+DROP TABLE IF EXISTS t_vo_acl;
+DROP TABLE IF EXISTS t_group_members;
+DROP TABLE IF EXISTS t_se_old;
+DROP TABLE IF EXISTS t_link_config_old;
+DROP TABLE IF EXISTS t_debug;
+DROP TABLE IF EXISTS t_optimize;
+DROP TABLE IF EXISTS t_optimize_mode;
+DROP TABLE IF EXISTS t_optimize_streams;
+DROP TABLE IF EXISTS t_optimize_active;
+
 INSERT INTO t_schema_vers (major, minor, patch, message)
-VALUES (8, 0, 0, 'FTS-1702: Schema changes for the eviction feature');
+VALUES (8, 0, 0, 'FTS v3.12.0 schema changes');
