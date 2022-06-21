f646f810df9ca5bd12ba8cb669790c2c2d6a683c
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-06-21 14:39:04 +0200

    FTS-1806: Re-arrange the order of SQL statements in schema 8.0.0 diff

--- src/db/schema/mysql/fts-diff-8.0.0.sql
+++ src/db/schema/mysql/fts-diff-8.0.0.sql
@@ -2,11 +2,12 @@
 -- FTS3 Schema 8.0.0
 -- [FTS-1709] Schema changes for SWIFT support
 -- [FTS-1744] Schema changes for the eviction feature
--- [FTS-1759] Add primary key on t_schema_vers table
+-- [FTS-1758] Add primary key on "t_schema_vers" table
 -- [FTS-1768] SRM TURL protocol configurable per link
 -- [FTS-1786] Add "t_gridmap" table
 -- [FTS-1804] Add "staging_metadata" field to "t_file" table
 -- [FTS-1805] Remove "v_staging" view
+-- Cleanup old tables
 --
 
 ALTER TABLE `t_se`
@@ -21,20 +22,20 @@ ALTER TABLE `t_job`
 ALTER TABLE `t_job_backup`
     ADD COLUMN `os_project_id` varchar(512) DEFAULT NULL;
 
-CREATE TABLE `t_gridmap` (
-    `dn` VARCHAR(255) NOT NULL,
-    `vo` VARCHAR(100) NOT NULL,
-    PRIMARY KEY(`dn`)
-);
-
-ALTER TABLE `t_schema_vers`
-    ADD PRIMARY KEY(`major`, `minor`, `patch`);
-
 ALTER TABLE `t_file`
     ADD COLUMN `staging_metadata` text;
 
 ALTER TABLE `t_file_backup`
     ADD COLUMN `staging_metadata` text;
+
+ALTER TABLE `t_schema_vers`
+    ADD PRIMARY KEY(`major`, `minor`, `patch`);
+
+CREATE TABLE `t_gridmap` (
+    `dn` VARCHAR(255) NOT NULL,
+    `vo` VARCHAR(100) NOT NULL,
+    PRIMARY KEY(`dn`)
+);
 
 DROP VIEW IF EXISTS `v_staging`;
 
