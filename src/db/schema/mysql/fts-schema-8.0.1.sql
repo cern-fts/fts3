226517a4d71d8c968a5f0f3b8513970fa44917e2
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-07-13 14:21:52 +0200

    FTS-1806: Add default configuration values to schema 8.0.1

--- src/db/schema/mysql/fts-schema-8.0.1.sql
+++ src/db/schema/mysql/fts-schema-8.0.1.sql
@@ -526,6 +526,8 @@ CREATE TABLE `t_link_config` (
   UNIQUE KEY `symbolic_name` (`symbolic_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO t_link_config (source_se, dest_se, symbolic_name, min_active, max_active, optimizer_mode, nostreams, no_delegation)
+VALUES ('*', '*', '*', 2, 130, 2, 0, 'off');
 
 --
 -- Table structure for table `t_oauth2_apps`
@@ -654,6 +656,8 @@ CREATE TABLE `t_schema_vers` (
   PRIMARY KEY (`major`,`minor`,`patch`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO t_schema_vers (major, minor, patch, message)
+VALUES (8, 0, 1, 'Schema 8.0.1');
 
 --
 -- Table structure for table `t_se`
@@ -677,6 +681,8 @@ CREATE TABLE `t_se` (
   PRIMARY KEY (`storage`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO t_se (storage, inbound_max_active, outbound_max_active)
+VALUES ('*', 200, 200);
 
 --
 -- Table structure for table `t_server_config`
@@ -695,6 +701,8 @@ CREATE TABLE `t_server_config` (
   `show_user_dn` varchar(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO t_server_config (vo_name)
+VALUES ('*');
 
 --
 -- Table structure for table `t_share_config`
