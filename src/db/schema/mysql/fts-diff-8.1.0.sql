09c7d39256c771fd1ae67cd264a2ac115e4209b3
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2023-07-21 17:12:00 +0200

    FTS-1920, FTS-1940: Schema for transfer retry log file

--- src/db/schema/mysql/fts-diff-8.1.0.sql
+++ src/db/schema/mysql/fts-diff-8.1.0.sql
@@ -1,10 +1,15 @@
 --
 -- FTS3 Schema 8.1.0
--- [FTS-1914] Schema changes for TPC-support configuration per SE
+-- [FTS-1914] TPC-support configuration per SE
+-- [FTS-1920] Retry log file
 --
 
 ALTER TABLE `t_se`
     ADD COLUMN `tpc_support` VARCHAR(10) DEFAULT NULL;
 
+ALTER TABLE `t_file_retry_errors`
+    ADD COLUMN `transfer_host` varchar(255) DEFAULT NULL,
+    ADD COLUMN `log_file` varchar(2048) DEFAULT NULL;
+
 INSERT INTO t_schema_vers (major, minor, patch, message)
-VALUES (8, 1, 0, 'FTS-1914: Schema changes for TCP-support configuration per SE');
+VALUES (8, 1, 0, 'FTS-1914: TPC-support configuration per SE');
