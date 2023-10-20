8507e24002180a4de733d0eaf978211cda7e0848
Steven Murray <Steven.Murray@cern.ch>
2023-10-20 09:32:26 +0000

    FTS-1928: Add TOKEN_PREP state to t_file.file_state

--- src/db/schema/mysql/fts-diff-9.0.0.sql
+++ src/db/schema/mysql/fts-diff-9.0.0.sql
@@ -15,7 +15,8 @@ ALTER TABLE `t_file`
   ADD COLUMN `src_token_id` char(16) DEFAULT NULL,
   ADD COLUMN `dst_token_id` char(16) DEFAULT NULL,
   ADD CONSTRAINT `src_token_id` FOREIGN KEY (`src_token_id`) REFERENCES `t_token` (`token_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `dst_token_id` FOREIGN KEY (`dst_token_id`) REFERENCES `t_token` (`token_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `dst_token_id` FOREIGN KEY (`dst_token_id`) REFERENCES `t_token` (`token_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  MODIFY `file_state` ENUM('STAGING','ARCHIVING','QOS_TRANSITION','QOS_REQUEST_SUBMITTED','STARTED','SUBMITTED','READY','ACTIVE','FINISHED','FAILED','CANCELED','NOT_USED','FORCE_START','ON_HOLD','ON_HOLD_STAGING', 'TOKEN_PREP') NOT NULL;
 
 INSERT INTO t_schema_vers (major, minor, patch, message)
 VALUES (9, 0, 0, 'FTS-1925: Full OAuth2 capabilities in FTS for submission, transfers and tape operations');
