56eec7e97185e34419db5ed2954ff156ae10c93a
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-12-13 14:42:35 +0100

    CI: Use last commit timestamp on testing rpms name

--- ci/common-rpm-build.sh
+++ ci/common-rpm-build.sh
@@ -12,7 +12,7 @@ function print_info {
   printf "======================\n"
 }
 
-TIMESTAMP=`date +%y%m%d%H%M`
+TIMESTAMP=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%y%m%d%H%M)
 GITREF=`git rev-parse --short HEAD`
 RELEASE=r${TIMESTAMP}git${GITREF}
 BUILD="devel"
