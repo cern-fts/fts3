f69e9d632f7bfe9b2600c735b85a71d6a18a3ac6
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2024-08-15 09:49:37 +0200

    Add the "ExperimentalPostgresSupport" configuration option to the Optimizer daemon as well

--- src/optimizer/main.cpp
+++ src/optimizer/main.cpp
@@ -63,6 +63,16 @@ static void initializeDatabase()
     auto dbConnectString = ServerConfig::instance().get<std::string>("DbConnectString");
     int pooledConn = ServerConfig::instance().get<int>("DbThreadsNum");
 
+    auto experimentalPostgresSupport =
+        ServerConfig::instance().get<std::string>("ExperimentalPostgresSupport");
+
+    if (dbType == "postgresql" && experimentalPostgresSupport != "true") {
+        throw std::runtime_error(
+            "Failed to initialize database: "
+            "DbType cannot be set to postgresql if ExperimentalPostgresSupport is not set to true"
+        );
+    }
+
     db::DBSingleton::instance().getDBObjectInstance()->init(
         dbType, dbUserName, dbPassword, dbConnectString, pooledConn);
 }
