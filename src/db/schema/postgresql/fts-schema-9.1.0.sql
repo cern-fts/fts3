93d37bc9d90760aa975e3a766c8f5d11dd9a9e69
Steven Murray <Steven.Murray@cern.ch>
2024-10-15 14:46:17 +0200

    Add file_transfer_active() to postgresql/fts-schema-9.1.0.sql

--- src/db/schema/postgresql/fts-schema-9.1.0.sql
+++ src/db/schema/postgresql/fts-schema-9.1.0.sql
@@ -1206,3 +1206,83 @@ BEGIN
     RETURN;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE file_transfer_active(
+    _active_file_id bigint,
+    _reason varchar,
+    _transfer_host varchar,
+    _pid integer,
+    _filesize bigint,
+    _tx_duration double precision,
+    _throughput real,
+    _current_failures integer,
+    _start_time timestamp without time zone,
+    _file_metadata varchar
+)
+AS $$
+DECLARE
+    _file_changed boolean = FALSE;
+    _file_row_file_state enum_file_state;
+BEGIN
+    SELECT
+        file_state
+    INTO
+        _file_row_file_state
+    FROM
+        t_file
+    WHERE
+        file_id = _active_file_id
+    FOR UPDATE;
+
+    IF NOT FOUND THEN
+        RAISE 'file_transfer_active failed: No file: file_id=%',
+            _active_file_id;
+    END IF;
+
+    IF _file_row_file_state != 'READY' THEN
+        RAISE 'file_transfer_active failed: Initial file state is not READY: file_id=% file_state=%',
+            _active_file_id, _file_row_file_state;
+    END IF;
+
+    SELECT change_file_state_and_queues(
+        _file_id => _active_file_id,
+        _curr_file_state => 'READY',
+        _next_file_state => 'ACTIVE'
+    ) INTO _file_changed;
+
+    IF NOT _file_changed THEN
+       RAISE 'file_transfer_active failed: Failed to change file state: file_id=%',
+           _active_file_id;
+    END IF;
+
+    IF LENGTH(_file_metadata) > 0 THEN
+        UPDATE t_file SET
+            reason = _reason,
+            transfer_host = _transfer_host,
+            pid = _pid,
+            filesize = _filesize,
+            tx_duration = _tx_duration,
+            throughput = _throughput,
+            current_failures = _current_failures,
+            start_time = _start_time,
+            file_metadata = _file_metadata
+        WHERE
+            file_id = _active_file_id;
+    ELSE
+        UPDATE t_file SET
+            pid = _pid,
+            filesize = _filesize,
+            tx_duration = _tx_duration,
+            throughput = _throughput,
+            current_failures = _current_failures,
+            start_time = _start_time
+        WHERE
+            file_id = _active_file_id;
+    END IF;
+
+    IF NOT FOUND THEN
+        RAISE 'file_transfer_active failed: Failed to update file: file_id=%',
+            _active_file_id;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
