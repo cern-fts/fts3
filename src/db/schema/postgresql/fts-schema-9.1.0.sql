cbd35edf1c4ee3beab7408d8eec0db6d7af66afd
Steven Murray <Steven.Murray@cern.ch>
2024-10-16 18:45:47 +0200

    Add file_transfer_failed() to postgresql/fts-schema-9.1.0.sql

--- src/db/schema/postgresql/fts-schema-9.1.0.sql
+++ src/db/schema/postgresql/fts-schema-9.1.0.sql
@@ -1366,3 +1366,87 @@ BEGIN
     END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE file_transfer_failed(
+    _failed_file_id bigint,
+    _reason varchar,
+    _transfer_host varchar,
+    _pid integer,
+    _filesize bigint,
+    _tx_duration double precision,
+    _throughput real,
+    _current_failures integer,
+    _finish_time timestamp without time zone,
+    _file_metadata varchar
+)
+AS $$
+DECLARE
+    _file_changed boolean = FALSE;
+    _file_row_file_state enum_file_state;
+BEGIN
+    SELECT
+        file_state
+    INTO
+        _file_row_file_state
+    FROM
+        t_file
+    WHERE
+        file_id = _failed_file_id
+    FOR UPDATE;
+
+    IF NOT FOUND THEN
+        RAISE 'file_transfer_failed failed: No file: file_id=%',
+            _failed_file_id;
+    END IF;
+
+    IF _file_row_file_state NOT IN ('READY', 'ACTIVE') THEN
+        RAISE 'file_transfer_failed failed: Initial file state is not READY or ACTIVE: file_id=% file_state=%',
+            _failed_file_id, _file_row_file_state;
+    END IF;
+
+    SELECT change_file_state_and_queues(
+        _file_id => _failed_file_id,
+        _curr_file_state => _file_row_file_state,
+        _next_file_state => 'FAILED'
+    ) INTO _file_changed;
+
+    IF NOT _file_changed THEN
+       RAISE 'file_transfer_failed failed: Failed to change file state: file_id=%',
+           _failed_file_id;
+    END IF;
+
+    IF LENGTH(_file_metadata) > 0 THEN
+        UPDATE t_file SET
+            reason = _reason,
+            transfer_host = _transfer_host,
+            pid = _pid,
+            filesize = _filesize,
+            tx_duration = _tx_duration,
+            throughput = _throughput,
+            current_failures = _current_failures,
+            finish_time = _finish_time,
+            dest_surl_uuid = NULL,
+            transferred = 0,
+            file_metadata = _file_metadata
+        WHERE
+            file_id = _failed_file_id;
+    ELSE
+        UPDATE t_file SET
+            pid = _pid,
+            filesize = _filesize,
+            tx_duration = _tx_duration,
+            throughput = _throughput,
+            current_failures = _current_failures,
+            finish_time = _finish_time,
+            dest_surl_uuid = NULL,
+            transferred = 0
+        WHERE
+            file_id = _failed_file_id;
+    END IF;
+
+    IF NOT FOUND THEN
+        RAISE 'file_transfer_failed failed: Failed to update file: file_id=%',
+            _failed_file_id;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
