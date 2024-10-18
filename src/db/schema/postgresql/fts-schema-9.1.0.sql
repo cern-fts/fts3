9bf0ddbe03ec679b5a99e7041a3eed6d623a0e2c
Steven Murray <Steven.Murray@cern.ch>
2024-10-18 20:01:38 +0200

    Replace _staging_start_file_id with _file_id

--- src/db/schema/postgresql/fts-schema-9.1.0.sql
+++ src/db/schema/postgresql/fts-schema-9.1.0.sql
@@ -1531,7 +1531,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE file_transfer_staging_start(
-    _staging_start_file_id bigint,
+    _file_id bigint,
     _reason varchar,
     _transfer_host varchar,
     _pid integer,
@@ -1554,24 +1554,24 @@ BEGIN
     FROM
         t_file
     WHERE
-        file_id = _staging_start_file_id
+        file_id = _file_id
     FOR UPDATE;
 
     IF NOT FOUND THEN
         RAISE 'file_transfer_staging_start failed: No file: file_id=%',
-            _staging_start_file_id;
+            _file_id;
     END IF;
 
     IF _file_row_file_state != 'STAGING' THEN
         SELECT change_file_state_and_queues(
-            _file_id => _staging_start_file_id,
+            _file_id => _file_id,
             _curr_file_state => _file_row_file_state,
             _next_file_state => 'STAGING'
         ) INTO _file_changed;
 
         IF NOT _file_changed THEN
             RAISE 'file_transfer_staging_start failed: Failed to change file state: file_id=%',
-                _staging_start_file_id;
+                _file_id;
         END IF;
     END IF;
 
@@ -1587,7 +1587,7 @@ BEGIN
             staging_start = _staging_start,
             file_metadata = _file_metadata
         WHERE
-            file_id = _staging_start_file_id;
+            file_id = _file_id;
     ELSE
         UPDATE t_file SET
             pid = _pid,
@@ -1597,12 +1597,12 @@ BEGIN
             current_failures = _current_failures,
             staging_start = _staging_start
         WHERE
-            file_id = _staging_start_file_id;
+            file_id = _file_id;
     END IF;
 
     IF NOT FOUND THEN
         RAISE 'file_transfer_staging_start failed: Failed to update file: file_id=%',
-            _staging_start_file_id;
+            _file_id;
     END IF;
 END;
 $$ LANGUAGE plpgsql;
