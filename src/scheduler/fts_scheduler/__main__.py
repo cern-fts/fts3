006f887d94dd3d8a63fb1abd83c71e92f3591be6
Steven Murray <Steven.Murray@cern.ch>
2024-12-18 11:14:25 +0100

    FTS4: Improve pylint score of fts_scheduler/__main__.py

--- src/scheduler/fts_scheduler/__main__.py
+++ src/scheduler/fts_scheduler/__main__.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
+"""
+Main entry point of the file-transfer scheduler daemon
+"""
 
 import argparse
 import configparser
@@ -10,11 +12,15 @@ import socket
 import sys
 import time
 
+from typing import Any
 from db import DbConnPool
 from scheduler import Scheduler
 
 
 def get_log(log_file_path, log_program_name, log_level):
+    """
+    Creates and returns the program's logger object
+    """
     hostname = socket.gethostname()
 
     log_format = (
@@ -30,84 +36,83 @@ def get_log(log_file_path, log_program_name, log_level):
     log_dir = os.path.dirname(log_file_path)
     if not os.path.isdir(log_dir):
         raise Exception(
-            "The logging directory {} is not a directory or does not exist".format(
-                log_dir
-            )
+            f"Logging directory is not a directory or does not exist: path={log_dir}"
         )
     if not os.access(log_dir, os.W_OK):
-        raise Exception("The logging directory {} cannot be written to".format(log_dir))
+        raise Exception(f"Logging directory cannot be written to: path={log_dir}")
 
     log_handler = logging.handlers.TimedRotatingFileHandler(
         filename=log_file_path, when="midnight", backupCount=30
     )
     log_handler.setFormatter(log_formatter)
 
-    log = logging.getLogger()
-    log.setLevel(log_level)
-    log.addHandler(log_handler)
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logger.addHandler(log_handler)
 
-    return log
+    return logger
 
 
-def get_config(path, program_name):
+def get_config(path, log_file_fallback) -> dict[str, Any]:
+    """
+    Parses the configuration file at the specified path and returns the result as a dict
+    """
     if not os.path.isfile(path):
-        print(f"The {program_name} configuration file does not exist: path={path}")
-        exit(1)
+        print(f"Configuration file does not exist: path={path}")
+        sys.exit(1)
 
-    config = configparser.ConfigParser()
-    config.read(path)
+    config_parser = configparser.ConfigParser()
+    config_parser.read(path)
 
-    if not config.has_option("database", "user"):
+    if not config_parser.has_option("database", "user"):
         raise Exception(
             f"Missing configuration option: option=database.user path={path}"
         )
 
-    if not config.has_option("database", "password"):
+    if not config_parser.has_option("database", "password"):
         raise Exception(
             f"Missing configuration option: option=database.password path={path}"
         )
 
-    if not config.has_option("database", "dbname"):
+    if not config_parser.has_option("database", "dbname"):
         raise Exception(
             f"Missing configuration option: option=database.dbname path={path}"
         )
 
-    if not config.has_option("database", "host"):
+    if not config_parser.has_option("database", "host"):
         raise Exception(
             f"Missing configuration option: option=database.host path={path}"
         )
 
-    if not config.has_option("database", "port"):
+    if not config_parser.has_option("database", "port"):
         raise Exception(
             f"Missing configuration option: option=database.port path={path}"
         )
 
-    if config.has_option("log", "level"):
+    if config_parser.has_option("log", "level"):
         valid_log_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-        log_level = config.get("log", "level")
+        log_level = config_parser.get("log", "level")
         if log_level not in valid_log_levels:
             raise Exception(
                 f"Invalid database log-level: permitted={valid_log_levels} actual={log_level}"
             )
 
     result = {
-        "log_file": config.get(
-            "log", "file", fallback=f"/var/log/fts4/{program_name}.log"
-        ),
-        "log_level": config.get("log", "level", fallback="INFO"),
-        "sec_between_executions": config.getint(
+        "log_file": config_parser.get("log", "file", fallback=log_file_fallback),
+        "log_level": config_parser.get("log", "level", fallback="INFO"),
+        "sec_between_executions": config_parser.getint(
             "main", "sec_between_executions", fallback=2
         ),
-        "db_user": config.get("database", "user"),
-        "db_password": config.get("database", "password"),
-        "db_dbname": config.get("database", "dbname"),
-        "db_host": config.get("database", "host"),
-        "db_port": config.getint("database", "port"),
-        "db_sslmode": config.get("database", "sslmode", fallback="require"),
-        "scheduler_algo_module_name": config.get(
+        "db_user": config_parser.get("database", "user"),
+        "db_password": config_parser.get("database", "password"),
+        "db_dbname": config_parser.get("database", "dbname"),
+        "db_host": config_parser.get("database", "host"),
+        "db_port": config_parser.getint("database", "port"),
+        "db_sslmode": config_parser.get("database", "sslmode", fallback="require"),
+        "scheduler_algo_module_name": config_parser.get(
             "scheduler", "algo_module_name", fallback="default_scheduler_algo"
         ),
-        "scheduler_algo_class_name": config.get(
+        "scheduler_algo_class_name": config_parser.get(
             "scheduler", "algo_class_name", fallback="DefaultSchedulerAlgo"
         ),
     }
@@ -115,40 +120,45 @@ def get_config(path, program_name):
     return result
 
 
-def get_major_db_schema_version(dbconn):
-    with dbconn.cursor() as cursor:
+def get_major_db_schema_version(param_dbconn):
+    """
+    Returns the major component of the database schema version
+    """
+    with param_dbconn.cursor() as cursor:
         cursor.execute("SELECT MAX(major) FROM  t_schema_vers")
         rows = cursor.fetchall()
         major_schema_version = rows[0][0]
         return major_schema_version
 
 
-program_name = "fts_scheduler"
+PROGRAM_NAME = "fts_scheduler"
 
 parser = argparse.ArgumentParser(description="Schedules FTS transfer jobs")
 parser.add_argument(
     "-c",
     "--config",
-    default=f"/etc/fts4/{program_name}.ini",
+    default=f"/etc/fts4/{PROGRAM_NAME}.ini",
     help="Path of the configuration file",
 )
 
 cmd_line = parser.parse_args()
 
-config = get_config(cmd_line.config, program_name)
+config = get_config(
+    cmd_line.config, log_file_fallback=f"/var/log/fts4/{PROGRAM_NAME}.log"
+)
 
 log = get_log(
     log_file_path=config["log_file"],
-    log_program_name=program_name,
+    log_program_name=PROGRAM_NAME,
     log_level=config["log_level"],
 )
 
 log.info("Started")
 
-nb_dbconns = 1
+NB_DBCONNS = 1
 dbconn_pool = DbConnPool(
-    min_conn=nb_dbconns,
-    max_conn=nb_dbconns,
+    min_conn=NB_DBCONNS,
+    max_conn=NB_DBCONNS,
     host=config["db_host"],
     port=config["db_port"],
     db_name=config["db_dbname"],
@@ -157,17 +167,17 @@ dbconn_pool = DbConnPool(
     sslmode=config["db_sslmode"],
 )
 
-min_allowed_major_db_schema_version = 9
+MIN_ALLOWED_MAJOR_DB_SCHEMA_VERSION = 9
 with dbconn_pool.get_dbconn() as dbconn:
     actual_major_db_schema_version = get_major_db_schema_version(dbconn)
-if actual_major_db_schema_version < min_allowed_major_db_schema_version:
+if actual_major_db_schema_version < MIN_ALLOWED_MAJOR_DB_SCHEMA_VERSION:
     print(
         f"Aborting: "
         "Database major version number is less than required: "
-        f"min_allowed={min_allowed_major_db_schema_version} "
+        f"min_allowed={MIN_ALLOWED_MAJOR_DB_SCHEMA_VERSION} "
         f"actual={actual_major_db_schema_version}"
     )
-    exit(1)
+    sys.exit(1)
 
 try:
     sys.path.append("/usr/share/fts/fts_scheduler_algo")
@@ -175,21 +185,21 @@ try:
         scheduler_algo_module = importlib.import_module(
             config["scheduler_algo_module_name"]
         )
-    except Exception as e:
-        raise Exception(f"Failed to import module: {e}")
+    except Exception as ex:
+        raise Exception(f"Failed to import module: {ex}") from ex
 
     SchedulerAlgoClass = getattr(
         scheduler_algo_module, config["scheduler_algo_class_name"], None
     )
     if SchedulerAlgoClass is None:
         raise Exception("Failed to find class")
-except Exception as e:
+except Exception as ex:
     raise Exception(
         "Failed to import the scheduler-algorithm class: "
         f"module={config['scheduler_algo_module_name']} "
         f"class={config['scheduler_algo_class_name']}: "
-        f"{e}"
-    )
+        f"{ex}"
+    ) from ex
 
 scheduler = Scheduler(log, SchedulerAlgoClass)
 
@@ -199,8 +209,8 @@ while True:
     try:
         with dbconn_pool.get_dbconn() as dbconn:
             scheduler.schedule(dbconn)
-    except Exception as e:
-        log.error(f"Caught exception when trying to schedule: {e}")
+    except Exception as ex:  # pylint:disable=broad-except
+        log.error("Caught exception when trying to schedule: %s", str(ex))
         import traceback
 
         traceback.print_exc()
