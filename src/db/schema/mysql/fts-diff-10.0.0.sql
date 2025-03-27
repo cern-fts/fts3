daa8ca543707d3eeb4ccbf6afadb20b31ae2c722
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2025-03-27 17:00:58 +0100

    FTS-2132: Move the "idx_staging_token" from database schema v9.2.0 to v10.0.0

--- src/db/schema/mysql/fts-diff-10.0.0.sql
+++ src/db/schema/mysql/fts-diff-10.0.0.sql
@@ -3,12 +3,16 @@
 -- [FTS-2131] Create database schema v10.0.0
 -- [FTS-1925] Full OAuth2 capabilities in FTS for submission, transfers and tape operations
 -- [FTS-1990] Implement Token Refresh service in FTS Server
+-- [FTS-2132] Index to optimize the staging requests count query
 --
 
 ALTER TABLE `t_token`
   ADD COLUMN `marked_for_refresh` tinyint(1) NULL DEFAULT 0,
-  ADD COLUMN `refresh_message` varchar(204) NULL DEFAULT NULL,
+  ADD COLUMN `refresh_message` varchar(2048) NULL DEFAULT NULL,
   ADD COLUMN `refresh_timestamp` timestamp NULL DEFAULT NULL;
+
+ALTER TABLE `t_file`
+    ADD INDEX `idx_staging_token` (`file_state`, `vo_name`, `source_se`, `bringonline_token`);
 
 INSERT INTO t_schema_vers (major, minor, patch, message)
 VALUES (10, 0, 0, 'FTS v3.14.0 schema changes');
