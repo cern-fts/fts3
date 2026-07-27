e0581eb0a40f6c5189aa638b4df66587222a9650
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2026-07-27 17:05:30 +0200

    FTS-2399: Allow retrying of stalled transfers detected by the Canceler Service (configurable option, default off)

--- src/config/ServerConfigReader.cpp
+++ src/config/ServerConfigReader.cpp
@@ -178,6 +178,11 @@ po::options_description ServerConfigReader::_defineConfigOptions()
         "Timeout for stalled transfers, in seconds"
     )
     (
+        "RetryStalledTransfers",
+        po::value<std::string>( &(_vars["RetryStalledTransfers"]) )->default_value("false"),
+        "Retry stalled transfers identified by the CancelerService"
+    )
+    (
         "MinRequiredFreeRAM",
         po::value<std::string>( &(_vars["MinRequiredFreeRAM"]) )->default_value("50"),
         "Minimum amount of free RAM in MB required for FTS3 to not go into auto-drain mode"
